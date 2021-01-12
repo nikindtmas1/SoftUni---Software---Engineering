8ba432006a0fbf4aea4813ca255b2b189b880164
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-12 09:53:11 +0200

    Added Programming Advanced/Syntax,Functions and Statements/demo.js

---
+++ Programming Advanced/Syntax,Functions and Statements/demo.js
@@ -0,0 +1,18 @@
+function demonstration(){
+
+    let name = 'Tomy';
+    console.log(name);
+
+    name = 'George';
+    console.log(name);
+
+    let cities = ['Varna', 'Sofiq', 'Russe', 'Burgas'];
+
+    console.log(cities.join());
+
+    cities[1] = 'Gabrovo'
+
+    console.log(cities.join());
+}
+
+demonstration();
---
+++ Programming Advanced/Syntax,Functions and Statements/echoFunction.js
@@ -0,0 +1,9 @@
+function echoFunction(arg){
+
+    console.log(arg.length);
+    console.log(arg);
+
+}
+
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
