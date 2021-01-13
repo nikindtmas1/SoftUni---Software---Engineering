59dd9a5942bafc3b6fc92bf0f5b39359579d397e
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-13 15:15:51 +0200

    Added decision task 6 exercises

---
+++ Programming Advanced/01.Exercise/06.js
@@ -0,0 +1,38 @@
+function cookingByNumbers(arg1, arg2, arg3, arg4, arg5, arg6) {
+
+    let startNum = Number(arg1);
+
+    let commands = [arg2, arg3, arg4, arg5, arg6];
+
+    for (let i = 0; i < commands.length; i++) {
+
+        let command = commands[i];
+
+        switch (command) {
+
+            case 'chop': startNum = startNum / 2;
+                console.log(startNum);
+                break;
+
+            case 'dice': startNum = Math.sqrt(startNum);
+                console.log(startNum);
+                break;
+
+            case 'spice': startNum += 1;
+                console.log(startNum);
+                break;
+
+            case 'bake': startNum = startNum * 3;
+                console.log(startNum);
+                break;
+
+            case 'fillet': startNum -= startNum * 0.20;
+                console.log(startNum);
+                break;
+        }
+    }
+
+
+}
+
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
