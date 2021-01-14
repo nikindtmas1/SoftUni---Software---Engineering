208ead940f246c4a95ba0721d90c7db89b6d0009
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-14 15:33:24 +0200

    Added decision 02 lab

---
+++ Programming Advanced/02.Lab/02.js
@@ -0,0 +1,38 @@
+function lastKNumbers(arg1, arg2) {
+
+    let n = Number(arg1);
+    let k = Number(arg2);
+
+    let result = [];
+
+    let length = n;
+
+    let firstNum = 1;
+
+    result.push(firstNum);
+
+    let index = 1;
+
+    while (index < length) {
+
+        let sum = 0;
+
+        for (let i = index - k; i < index; i++) {
+
+            if (i >= 0) {
+
+                sum += result[i];
+            }
+
+
+        }
+
+        result.push(sum);
+
+        index++;
+    }
+
+    console.log(result.join(', '));
+}
+
+lastKNumbers(8, 2);
