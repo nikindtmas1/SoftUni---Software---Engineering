0d7c7db4a34c15e3642005883e054dec015aed6d
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-19 11:14:16 +0200

    Added obj method in demo.js

--- Programming Advanced/Labs/03.Lab/demo.js
+++ Programming Advanced/Labs/03.Lab/demo.js
@@ -9,12 +9,34 @@ function demo(){
         }
     };
 
-    console.log(obj.age(46));
     
     let {name, employeeCount} = obj;
-
+    /*
+    console.log(obj.age(46));
     console.log(name);
     console.log(employeeCount);
     
+    let num = 5;
+    
+    const parser = {
+        
+        increment(){return ++num; },
+        decrement(){ num--; },
+        reset(){ num = 0; }
+    }
+    
+    console.log( parser[reset]());
+    */
+
+     let myObj = {
+
+        firstName: 'Nikolai',
+        lastName: 'Nikolaev',
+        fullName() {
+            return this.firstName + ' ' + this.lastName;
+        }
+     }
+
+     console.log(myObj.fullName());
 }
 demo();
