7894b8d073bc8aa4b1857de31493bcdbb00f2d61
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-19 09:57:45 +0200

    Added method in demo.js

--- Programming Advanced/Labs/03.Lab/demo.js
+++ Programming Advanced/Labs/03.Lab/demo.js
@@ -3,9 +3,14 @@ function demo(){
     let obj = {
         name: 'engineering',
         director: 'Ted Thomson',
-        employeeCount: 25
+        employeeCount: 25,
+        age: function(arg){
+           return `My age is ${arg}`;
+        }
     };
 
+    console.log(obj.age(46));
+    
     let {name, employeeCount} = obj;
 
     console.log(name);
