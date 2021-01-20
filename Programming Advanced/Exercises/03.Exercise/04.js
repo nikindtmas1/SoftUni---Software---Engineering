c9ed5786193b3d45c94fcc22acfc04752b41aced
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-20 10:51:25 +0200

    Added decision Exercises/03.Exercise/04.js

---
+++ Programming Advanced/Exercises/03.Exercise/04.js
@@ -0,0 +1,36 @@
+function heroicInventory(input) {
+
+    let arrInput = [...input];
+    let output = [];
+
+    for (let i = 0; i < arrInput.length; i++) {
+        let register = {};
+
+        let currHero = arrInput[i].split(' / ');
+
+        [heroName, heroLevel, items] = currHero;
+
+        register['name'] = heroName;
+        register['level'] = Number(heroLevel);
+
+        if (items != undefined) {
+
+            let arrItems = items.split(', ');
+
+            register['items'] = arrItems;
+
+            output.push(register);
+        }
+    }
+    output = JSON.stringify(output);
+
+
+
+
+    return output;
+
+}
+
+console.log(heroicInventory(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']));
