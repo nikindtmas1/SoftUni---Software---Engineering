59be19b3dda7204efffe6cb857218d5336bc7936
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-23 23:01:48 +0200

    Added decision Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js

--- Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,68 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value.toLowerCase();
+  let command = document.getElementById('naming-convention').value;
+
+  let result = document.getElementById('result');
+
+  switch (command) {
+
+    case 'Pascal Case': result.textContent = upperCase(input);
+      break;
+
+    case 'Camel Case': result.textContent = camelCase(input);
+      break;
+
+    default: result.textContent = 'Error!';
+      break;
+  }
+
+  function upperCase(string) {
+
+    let str = string.slice();
+
+    let output = '';
+
+    let index = 0;
+    output += str[index].toUpperCase();
+    index++;
+
+    while (index < str.length) {
+
+      if (str[index] !== ' ') {
+        output += str[index];
+        index++;
+      } else if (str[index] === ' ') {
+        index++;
+        output += str[index].toUpperCase();
+        index++;
+      }
+    }
+
+    return output;
+  }
+
+  function camelCase(string) {
+
+    let str = string.slice();
+
+    let index = 0;
+    let output = '';
+
+    while (index < str.length) {
+
+      if (str[index] !== ' ') {
+        output += str[index];
+        index++;
+      } else if (str[index] === ' ') {
+        index++;
+        output += str[index].toUpperCase();
+        index++;
+      }
+
+    }
+    return output;
+
+  }
+
+
 }
