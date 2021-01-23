0bc8ac2c586ecfc0ebabfc89ae59de3c85c15673
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-23 17:29:55 +0200

    Change if / else Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/02. Accordion.gif

---
+++ Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -0,0 +1,19 @@
+function toggle() {
+    let button = document.querySelector('.button')
+    let divExtra = document.querySelector('#extra')
+    
+    if(divExtra.style.display === 'none' || divExtra.style.display === ''){
+        divExtra.style.display = 'block';
+    }else{
+        divExtra.style.display = 'none';
+    }
+
+    if(button.textContent === 'More'){
+        button.textContent = 'Less'
+    }else{
+        button.textContent = 'More';
+    }
+    // divExtra.style.display = divExtra.style.display === 'none' 
+    // || divExtra.style.display === '' ? 'block' : 'none';
+    // button.textContent = button.textContent === 'More' ? 'Less' : 'More';
+}
