f1c2ee183697911fd65152555bcdec8896da8c5d
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-25 19:24:22 +0200

    Added decision lab 05

--- Programming Advanced/Labs/05.Lab/01. List-Of-Items/app.js
+++ Programming Advanced/Labs/05.Lab/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText').value;
+
+    let liElement = document.createElement('li');
+
+    liElement.appendChild(document.createTextNode(text));
+
+
+    document.getElementById('items').appendChild(liElement);
+
+    document.getElementById('newItemText').value = '';
 }
