6375196d3cc24d7349a677c56ee36dd7405ba8cb
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-25 14:41:18 +0200

    Added decision Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/.vscode/settings.json

--- Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-  //TODO
+  const btn = document.getElementById('formatItBtn');
+  let input = document.getElementById('input');
+  let output = document.getElementById('output');
+  let inputText = input.value;
+  btn.click();
+  let inputArr = inputText.split('.').filter((x) => x != '');
+  let result = [];
+  let final = [];
+
+  if (inputArr.length <= 3 && inputArr.length !== 0) {
+    result.push(`<p>${inputArr}<p/>`);
+    output.textContent = result;
+  } else if (inputArr.length > 3) {
+
+    let index = 0;
+    // let currLetter = inputArr[index];
+    // result.push(currLetter);
+    // index++;
+    while (index < inputArr.length) {
+      currLetter = inputArr[index];
+      result.push(currLetter);
+      if ((index + 1) % 3 === 0) {
+        final.push(`<p>${result}</p>`);
+        //output.textContent = result;
+
+        result = [];
+      }
+
+      index++;
+
+    }
+
+    if (result.length > 0) {
+      final.push(`<p>${result}</p>`);
+
+    }
+
+    let print = final.join('\n');
+    output.textContent = print;
+
+  }
+
 }
--- Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ Programming Advanced/Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,71 +1,71 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-   const body = document.querySelector('tbody');
-   let input = document.querySelector('#searchField');
-   const bestResturantP = document.querySelector('#bestRestaurant>p');
-   const workarsP = document.querySelector('#workers>p');
+   document.querySelector('button#btnSend').addEventListener('click', onClick);
+   const input = document.querySelector('#inputs textarea');
+   const bestRestaurantP = document.querySelector('#outputs #bestRestaurant p');
+   const workersP = document.querySelector('#outputs #workers p');
 
-   function onClick () {
-     let arr = JSON.parse(input.value);
+   function onClick() {
 
-     let restourants = {};
-      
-     arr.forEach(line => {
-        const tokens = line.split(' - ');
+      let arr = JSON.parse(input.value);
 
-        const name = tokens[0];
-        const workersArr = tokens[1].split(', ');
-      let workars = [];
-      let averageSalary = 0;
-      let bestSalary = 0;
+      let restaurants = {};
 
-      for (const workar of workersArr) {
-         let workarTokens = worker.split(' ');
-         const salary = Number(workarTokens[1])
-         workars.push({
-            name: workarTokens[0],
-            salary
-         })
-      }
+      arr.forEach(line => {
+         const tokens = line.split(' - ');
+         const name = tokens[0];
+         const workersArr = tokens[1].split(', ');
 
-      if(restourants[name]){
-         workars.concat(restourants[name].workars)
+         let workers = [];
 
-      }
-
-         workars.sort((workar1, workar2) => workar2.salary - workar1.salary)
-
-         let bestSalary = workars[0].salary;
-         averageSalary = workars.reduce((sum, workar) => sum + workar.salary, 0) / workars.length;
-
-
-      restourants[name] = {
-         workars,
-         averageSalary,
-         bestSalary
-      }
-  
-     });
-
-     let bestRestorantSalary = 0;
-     let bestRestorant = undefined;
-     for (const name in restourants) {
-      
-      if(restourants[name].averageSalary > bestRestorantSalary){
-         bestRestorant = {name, ...restourants[name],
-            bestSalary: restourants[name].bestSalary,
-            averageSalary: restourants[name].averageSalary
+         for (const worker of workersArr) {
+            const workerTokens = worker.split(' ');
+            const salary = Number(workerTokens[1]);
+            workers.push({ name: workerTokens[0], salary });
          }
 
-         bestRestorantSalary = restourants[name].averageSalary
+         if (restaurants[name]) {
+            workers = workers.concat(restaurants[name].workers);
+         }
+
+         workers.sort((worker1, worker2) => worker2.salary - worker1.salary);
+
+         const bestSalary = workers[0].salary;
+         const averageSalary = workers.reduce((sum, worker) => sum + worker.salary, 0) / workers.length;
+
+         restaurants[name] = {
+            workers,
+            averageSalary,
+            bestSalary
+         }
+      });
+      //   TODO:
+      let bestRestaurantSalary = 0;
+      let best = undefined;
+
+      for (const name in restaurants) {
+         if (restaurants[name].averageSalary > bestRestaurantSalary) {
+            best = {
+               name,
+               workers: restaurants[name].workers,
+               bestSalary: restaurants[name].bestSalary,
+               averageSalary: restaurants[name].averageSalary
+            }
+
+            bestRestaurantSalary = restaurants[name].averageSalary;
+
+         }
       }
-     }
 
-     bestResturantP.textContent = `Name: ${best.name} Average Salary: ${best.averageSalary.toFixed(2)} 
-     Best Salary: ${best.bestSalary.toFixed(2)}`
 
-     let workarsresult = [];
+      bestRestaurantP.textContent = `Name: ${best.name} Average Salary: ${best.averageSalary.toFixed(2)} Best Salary: ${best.bestSalary.toFixed(2)}`;
 
-     best.workars.forEach()
+      let workersResult = [];
+
+      best.workers.forEach(worker => {
+         workersResult.push(`Name: ${worker.name} With Salary: ${worker.salary}`)
+      })
+
+      workersP.textContent = workersResult.join(' ');
+
    }
 }
