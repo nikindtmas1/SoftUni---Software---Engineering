fad483fb633c6dfa0bfd7d8dbd158ceb2ecc22b3
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-26 23:44:18 +0200

    Added decision

--- Programming Advanced/Exercises/05. Exercises/03. Locked-Profile/app.js
+++ Programming Advanced/Exercises/05. Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,48 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let test = document.getElementById('main');
+    test.addEventListener('click', function (event) {
+
+        const clickName = event.target.tagName
+
+        if (clickName === 'BUTTON') {
+            let locked = event.target.parentNode.querySelector('input[type=radio]:checked').value === 'lock';
+
+            if (locked === true) {
+
+                //console.log(locked);
+
+            } else {
+                //console.log(event.target.parentNode.querySelector('div'));
+                let div = event.target.parentNode.querySelector('div');
+                //console.log(div);
+                let isVisible = div.style.display === 'block';
+                //console.log(isVisible);
+                div.style.display = isVisible ? 'none' : 'block';
+                event.target.textContent = !isVisible ? 'Hide it' : 'Show more';
+
+            }
+
+        }
+    });
+
 }
+        /*
+        if(event.target.tagName === 'BUTTON'){
+
+            const locked = event.target.parentNode
+            .querySelector('input[type-radio]:checked').value === 'lock';
+
+            if(isLocked){
+
+                return
+            }
+
+            let div = event.target.parentNode.querySelector('div');
+
+           let isVisible = div.style.display === 'block';
+           div.style.display = isVisible ? 'none' : 'block'
+
+           event.target.textContent = !isVisible ? 'Hide it' : 'Show more'
+        }
+    })
+    */
--- Programming Advanced/Exercises/05. Exercises/06. Furniture/solution.js
+++ Programming Advanced/Exercises/05. Exercises/06. Furniture/solution.js
@@ -1,4 +1,53 @@
 function solve() {
+const textarea = document.querySelectorAll('textarea');
+const buttons = document.querySelectorAll('button');
 
+buttons[0].addEventListener('click', function (e){
+const arr =JSON.parse(textarea[0].value);
+
+
+for(let el of arr){
+const row = document.createElement('tr');
+const cellOne = document.createElement('td');
+const image = document.createElement('img');
+image.setAttribute('src', el.img);
+cellOne.appendChild(image);
+
+const cellTwo = document.createElement('td');
+
+const cellThri = document.createElement('td');
+const cellFour = document.createElement('td');
+const cellFif = document.createElement('td');
+
+
+}
+console.log(arr);
+})
+
+buttons[1].addEventListener('click', function (e){
+  const furniture = Array.from(body.querySelectorAll('input[type=checkbox]:checked'))
+  .map(input => input.parentNode.parentNode);
+
+  const result = {
+    bought: [],
+    totalPrice: 0,
+    decFactorSum: 0
+  }
+
+  for(let row of furniture){
+
+    const cells = row.children
+    const name = cells[1].children[0].textContent;
+    result.bought.push(name)
+
+    const price = Number(cells[2].children[0].textContent);
+    result.totalPrice += price;
+
+    const factor = Number(cells[3].children[0].textContent);
+    result.decFactorSum += factor;
+  }
+
+  textarea[1].value = 
+})
   //TODO...
 }
