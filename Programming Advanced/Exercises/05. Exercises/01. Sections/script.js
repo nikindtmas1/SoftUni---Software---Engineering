6d96cb6379642e293fd0fe59dc1007a0bbbd61e8
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-26 18:34:11 +0200

    Added decision

--- Programming Advanced/Exercises/05. Exercises/01. Sections/script.js
+++ Programming Advanced/Exercises/05. Exercises/01. Sections/script.js
@@ -1,3 +1,29 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content');
+
+   let inputArr = (words)
+
+   for (const el of inputArr) {
+
+      let div = document.createElement('div');
+      let pargraf = document.createElement('p');
+
+      pargraf.textContent = el;
+      pargraf.style.display = 'none'
+      div.appendChild(pargraf);
+
+      content.appendChild(div);
+
+
+      //console.log(div);
+   }
+
+   content.addEventListener('click', function (ev) {
+      if (ev.target.tagName === 'DIV' || ev.target.tagName === 'P') {
+         const p = ev.target.children[0] || ev.target;
+
+         const isVisible = p.style.display === 'block';
+         p.style.display = isVisible ? 'none' : 'block'
+      }
+   })
 }
