75d11cbc09bdf2b5b63cff94e0f942598dd8631a
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-28 14:31:09 +0200

    Added decision Exercises/05. Exercises/04. Fill-Dropdown/dropdown.js

--- Programming Advanced/Exercises/05. Exercises/04. Fill-Dropdown/dropdown.js
+++ Programming Advanced/Exercises/05. Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const fillds = Array.from(document.querySelectorAll('input[type=text]'));
+    const menu = document.getElementById('menu');
+
+    let inputText = fillds[0].value;
+    let inputValue = fillds[1].value;
+
+    let option = document.createElement('option');
+    option.textContent = inputText;
+    option.value = inputValue;
+
+    menu.appendChild(option);
+   
+    fillds[0].value = '';
+    fillds[1].value = '';
+    
 }
