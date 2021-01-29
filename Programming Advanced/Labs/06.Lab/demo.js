ab35c3668a23785174485eaed325f2e757e8043c
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-29 13:47:09 +0200

    Added template Exercises/06.Exercises/08. Next Article/index.html

--- Programming Advanced/Labs/06.Lab/demo.js
+++ Programming Advanced/Labs/06.Lab/demo.js
@@ -10,9 +10,25 @@ const obj = {
 
 //obj.outer()
 
-function greed(){
+
+function greet(){
     console.log(this.name);
 }
 
 const person = { name: 'Alex' }
-greed.call(person)
+//greet.call(person)
+
+const sharePersonalInfo = function(...activities){
+
+    let info = `Hello, my name is ${this.name}.\n`+`I'm a ${this.profession}.\n`;
+
+   let reducer = activities.reduce((acc, curr) => {
+        let el = `---${curr}\n`;
+        return acc + el;
+    }, "My hobbies are:\n").trim();
+
+    return info + reducer;
+}
+
+// const firstPerson = { name: "Peter", profession: "Fisherman" };
+// console.log(sharePersonalInfo.call(firstPerson, 'biking', 'swimming','football'));
