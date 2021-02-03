41bb443ef66eabbe819a1baabb968c6eb6f55a16
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-02-03 20:52:54 +0200

    Added Exercises/07.exercises/03/charLookup.test.js

--- Programming Advanced/Exercises/07.exercises/03/charLookup.test.js
+++ Programming Advanced/Exercises/07.exercises/03/charLookup.test.js
@@ -1,29 +1,22 @@
 const {expect} = require('chai');
-const charLookup = require('./charLookup');
+const lookupChar = require('./charLookup');
 
 describe('lookupChar', () => {
 
     it('FirstParam string', () => {
-        expect(charLookup(5, 2)).to.equal(undefined);
-    });
-    it('SecondParam number', () => {
-        expect(charLookup('str', 'str')).to.equal(undefined);
+        expect(lookupChar(5, 2)).to.equal(undefined);
+        expect(lookupChar('str', 'str')).to.equal(undefined);
     });
 
+    
     it('Index out of range', () => {
-        expect(charLookup('niki', 5)).to.equal('Incorrect index');
-    });
-
-    it('Index out of range', () => {
-        expect(charLookup('niki', 4)).to.equal('Incorrect index');
-    });
-
-    it('Index out of range', () => {
-        expect(charLookup('niki', -1)).to.equal('Incorrect index');
+        expect(lookupChar('niki', -1)).to.equal('Incorrect index');
+        expect(lookupChar('niki', 5)).to.equal('Incorrect index');
     });
 
     it('Char at string', () => {
-        expect(charLookup('niki', 2)).to.equal('k');
+        expect(lookupChar('niki', 2)).to.equal('k');
+        expect(lookupChar('niki', 0)).to.equal('n');
     });
 
 });
