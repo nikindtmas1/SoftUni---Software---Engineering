b5af2c196f764de3057eb06f0de1fa968c9890fb
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-02-03 17:10:19 +0200

    Added decision exercise 05

---
+++ Programming Advanced/Exercises/07.exercises/05.Notification/app.js
@@ -0,0 +1,14 @@
+function notify(message) {
+
+  let div = document.querySelector('#notification');
+  
+
+  div.textContent = message;
+  div.style.display = 'block';
+
+
+  div.addEventListener('click', (ev) => {
+  ev.target.style.display = 'none';  
+  });
+
+}
