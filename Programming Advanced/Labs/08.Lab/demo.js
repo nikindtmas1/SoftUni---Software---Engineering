33d79ce71b4ff246b9558a368597e8a9ee0d578b
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-02-04 14:57:32 +0200

    Added demo.js

---
+++ Programming Advanced/Labs/08.Lab/demo.js
@@ -0,0 +1,13 @@
+
+
+// let getRectArea = function(width, height){
+//     return width * height;
+//   };
+  
+//   console.log(getRectArea(3, 4));
+
+    hoisted(); // logs "foo"
+
+    function hoisted() {
+    console.log('foo');
+    }
