7d3b657cbf570ebff09ced14c0387ccb2c00f530
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-02-12 14:08:21 +0200

    Added decision Exercises/09.Exercises/03.js

---
+++ Programming Advanced/Exercises/09.Exercises/03.js
@@ -0,0 +1,18 @@
+function solve(){
+
+let proto = {};
+let inst = Object.create(proto);
+
+inst.extend = function(templates){
+Object.entries(templates).forEach(([key, value]) => {
+    if(typeof value === 'function'){
+
+        proto[key] = value;
+    }else{
+
+        inst[key] = value;
+    }
+});
+};
+return inst;
+}
