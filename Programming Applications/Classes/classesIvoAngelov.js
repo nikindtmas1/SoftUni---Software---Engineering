0fc4baae3c3e87e6f84217ec0ee406241c741f95
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-04-27 20:57:42 +0300

    Added change

--- Programming Applications/Classes/classesIvoAngelov.js
+++ Programming Applications/Classes/classesIvoAngelov.js
@@ -1,11 +1,16 @@
 class Person {
     constructor(name, age) {
-        this.name = name,
+        this.name = name
         this.age = age
+    }
+
+    get firstName() {
+       return this.name.split(' ')[0]
     }
 }
 
-const person = new Person( 'Nikolay', 47 );
+const person = new Person( 'Nikolay Nikolaev', 47 );
 
 console.log(person.name);
 console.log(person.age);
+console.log(person.firstName);
