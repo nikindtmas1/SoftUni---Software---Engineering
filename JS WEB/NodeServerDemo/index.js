a02e970e6599cee18f7e7971a88c9bc4ab00e4c0
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-05-31 23:14:11 +0300

    Added index code

--- JS WEB/NodeServerDemo/index.js
+++ JS WEB/NodeServerDemo/index.js
@@ -1,10 +1,24 @@
 const http = require('http');
 
+const port = 3000
 const server = http.createServer(requestHandler);
 
+const html = `
+<html>
+<header></header>
+<body>
+    <div>
+        <h1>My Page!</h1>
+        <p>Welcome to My Page!</p>
+    </div>
+</body>
+</html>`
+
 function requestHandler(req,res){
-    res.write('Hello world!');
+    console.log(">>> method: ", req.method);
+    console.log(">>> url: ", req.url);
+    res.write(html);
     res.end();
 }
 
-server.listen(3000);
+server.listen(port, () => console.log('Server listenning on port ' + port));
