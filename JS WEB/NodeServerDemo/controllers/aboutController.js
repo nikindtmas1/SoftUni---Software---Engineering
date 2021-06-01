4824d56e8a87e28b2aa7223d022533f38aa29ae5
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-01 22:31:45 +0300

    Added aboutController.js

---
+++ JS WEB/NodeServerDemo/controllers/aboutController.js
@@ -0,0 +1,14 @@
+const layout = require('../views/layout');
+
+const html = `
+
+    <div>
+        <h1>About Us</h1>
+        <p>About Page!</p>
+    </div>
+`
+
+module.exports = (req,res) => {
+    res.write(layout(html, 'About'));
+    res.end();
+}
