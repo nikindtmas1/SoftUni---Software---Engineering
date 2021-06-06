ced19a8fa40faccc811e98724817334a7bfce5fb
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-06 17:32:02 +0300

    Added code index.js

--- JS WEB/NodeServerDemo/index.js
+++ JS WEB/NodeServerDemo/index.js
@@ -3,10 +3,13 @@ const router = require('./router');
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
 
-router.registerHandler('/', homeController);
-router.registerHandler('/about', aboutController);
-router.registerHandler('/catalog', catalogController);
+router.get('/', homeController);
+router.get('/about', aboutController);
+router.get('/catalog', catalogController);
+
+router.post('/create', createController);
 
 const port = 3000
 const server = http.createServer(requestHandler);
