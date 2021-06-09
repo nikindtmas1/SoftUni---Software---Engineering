e44baf694f35f02f43117e0999243e4da066881b
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-09 21:10:17 +0300

    Added code index.js

--- JS WEB/NodeServerDemo/index.js
+++ JS WEB/NodeServerDemo/index.js
@@ -1,23 +1,30 @@
 const http = require('http');
+
 const router = require('./router');
+
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const catalogController = require('./controllers/catalogController');
 const createController = require('./controllers/createController');
+const deleteController = require('./controllers/deleteController');
 
 router.get('/', homeController);
 router.get('/about', aboutController);
 router.get('/catalog', catalogController);
 
 router.post('/create', createController);
+router.get('/delete', deleteController);
 
 const port = 3000
 const server = http.createServer(requestHandler);
 
 function requestHandler(req,res){
+    const url = new URL(req.url, 'http://localhost');
+    
     console.log(">>> method: ", req.method);
     console.log(">>> url: ", req.url);
-    const handler =  router.match(req.method,req.url);
+    
+    const handler =  router.match(req.method,url.pathname);
    handler(req,res);
 }
 
