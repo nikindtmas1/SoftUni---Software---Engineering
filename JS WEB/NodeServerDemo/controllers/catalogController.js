71ac064a0cbedff3b6ca98fff526c5ae99ad13de
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-09 21:09:53 +0300

    Added code catalogController.js

--- JS WEB/NodeServerDemo/controllers/catalogController.js
+++ JS WEB/NodeServerDemo/controllers/catalogController.js
@@ -10,7 +10,7 @@ const html = (items) => `
         <input type="submit" value="Create Item">
     </form>
     <ul>
-        ${items.map(([id,i]) => `<li data-id="${id}">${i.name} - ${i.serial}</li>`)}
+        ${items.map(([id,i]) => `<li data-id="${id}">${i.name} - ${i.serial}<a href="/delete?id=${id}">[Delete]</a></li>`).join('')}
     </ul>
 </div>
 `
