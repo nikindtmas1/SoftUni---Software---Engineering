313e06822d2196ea74050d87d6ca0fe57c384d50
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-15 22:11:53 +0300

    test 123

--- JS WEB/ExspressDemo/index.js
+++ JS WEB/ExspressDemo/index.js
@@ -6,4 +6,8 @@ app.get('/', (req,res) => {
    res.send('Hello express!');
 });
 
+app.get('/catalog', (req,res) => {
+    res.send('Catalog Page');
+})
+
 app.listen(3000, console.log('Server listening on port 3000'));
