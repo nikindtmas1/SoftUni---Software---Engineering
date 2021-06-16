86df69d3985b6c418bc89d87c96fae97cc3fef77
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-16 22:36:36 +0300

    Added catalogRouter

--- JS WEB/ExspressDemo/index.js
+++ JS WEB/ExspressDemo/index.js
@@ -4,11 +4,13 @@ const app = express();
 
 const port = 3000;
 
+const catalogRouter = require('./catalog');
+
 app.get('/', (req,res) => {
    res.send('Hello express!');
 });
 
-
+app.use(catalogRouter);
 
 
 app.listen(3000, () => console.log(`Server listening on port: ${port}`));
