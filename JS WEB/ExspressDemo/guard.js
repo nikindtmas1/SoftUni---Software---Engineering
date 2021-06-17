6a852766ad1769ddce5b372179ffd961cc5de820
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-17 21:22:13 +0300

    Added if else guard.js

--- JS WEB/ExspressDemo/guard.js
+++ JS WEB/ExspressDemo/guard.js
@@ -1,6 +1,12 @@
 function isAdmin(req, ers, next){
-    console.log(req.headers);
-    next();
+    if(req.headers.hasOwnProperty('x-admin')){
+        next();
+
+    }else{
+        res.status(401);
+        res.send('Admins only. Please sign in.');
+    }
+    
 }
 
 module.exports = isAdmin;
