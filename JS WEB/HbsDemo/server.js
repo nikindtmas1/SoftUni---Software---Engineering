b71d9ddbc07fadd0c002eb88443796fc3944dc67
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-20 17:09:19 +0300

    Added code

--- JS WEB/HbsDemo/server.js
+++ JS WEB/HbsDemo/server.js
@@ -3,3 +3,17 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
+const port = 3000;
+
+app.engine('handlebars', hbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.send('It\'is working!')
+});
+
+app.listen(port, console.log(`Server listening on port ${port}`))
+
