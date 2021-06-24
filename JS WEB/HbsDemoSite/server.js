8086feb695a944acfd04df3abeba2d3c7723f0e1
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-24 21:59:00 +0300

    Added server.js code router catalog

--- JS WEB/HbsDemoSite/server.js
+++ JS WEB/HbsDemoSite/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const home = require('./controllers/home');
+const catalog = require('./controllers/catalog');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.engine('.hbs', hbs({
 app.set('view engine', '.hbs');
 
 app.get('/', home);
+app.use('/catalog', catalog);
 
 app.use('/static', express.static('static'))
 app.listen(port, console.log(`Server listening on port ${port}`));
