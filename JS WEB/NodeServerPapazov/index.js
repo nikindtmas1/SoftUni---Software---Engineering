211957c9ed07b1df71a6c2b269cec99df6f0b1c7
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-26 22:39:45 +0300

    Added modules index.js

--- JS WEB/NodeServerPapazov/index.js
+++ JS WEB/NodeServerPapazov/index.js
@@ -1,15 +1,26 @@
 const http = require('http');
+const url = require('url');
+const log = require('./logger');
+const queryString = require('querystring');
 
 const logger = require('./logger');
 
 let name1 = 'Pesho';
+logger(name1);
 
+let urlObj = url.parse('https://softuni.bg/trainings/3223/js-back-end-january-2021#lesson-21963?year=2020&month=may')
 
+console.log(urlObj);
 
-http.createServer((req, res) => {
+const qr = queryString
+.parse('year=2020&month=may');
+
+console.log(qr.year);
+console.log(qr.month);
+/*http.createServer((req, res) => {
     res.write('Hello Node!');
     
     res.end();
-}).listen(3000); 
+}).listen(3000);
 
-console.log('Server listening on port 3000');
+console.log('Server listening on port 3000');*/
