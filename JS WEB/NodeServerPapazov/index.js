bd12b9edbb30aa8d20d756b01932222a34e8d625
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-27 18:49:33 +0300

    Added switch index.js

--- JS WEB/NodeServerPapazov/index.js
+++ JS WEB/NodeServerPapazov/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const querystring = require('querystring');
 
 const catalog = require('./views/catalog');
 const about = require('./views/about');
@@ -13,16 +14,21 @@ function requestHandler(req, res){
     console.log(req.url);
 
     let reqUrl = url.parse(req.url);
+    let params = querystring.parse(reqUrl.query);
 
-    console.log(reqUrl);
-    
-    if(req.url === '/'){
-        res.write(home);
-    }else if(req.url == '/catalog'){
-        res.write(catalog);
-    }else if(req.url == '/about'){
-        res.write(about);
+    console.log(reqUrl.pathname);
+    console.log(params);
+    switch(req.method){
+        case 'GET':
+            if(req.url === '/'){
+                res.write(home);
+            }else if(req.url == '/catalog'){
+                res.write(catalog);
+            }else if(req.url == '/about'){
+                res.write(about);
+            }
     }
+    
     
     res.end();
     
