cc7192d846b44990735a3144adea4f6dd5197d0f
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-27 16:59:00 +0300

    Create catalog.js

--- JS WEB/NodeServerPapazov/index.js
+++ JS WEB/NodeServerPapazov/index.js
@@ -1,12 +1,12 @@
 const http = require('http');
-
+const catalog = require('./views/catalog');
 const port = 3000;
 
 function requestHandler(req, res){
     if(req.url === '/'){
         res.write('Hello Node!');
     }else if(req.url == '/catalog'){
-        res.write('Catalog Page!');
+        res.write(catalog);
     }else if(req.url == '/about'){
         res.write('About Page!');
     }
---
+++ JS WEB/NodeServerPapazov/views/catalog.js
@@ -0,0 +1,1 @@
+module.exports = `<h1>Catalog Page!</h1>`
