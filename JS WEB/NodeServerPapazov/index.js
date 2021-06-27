c9c6f3625cf0d46997903ad2a315de7492fc03d2
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-27 17:03:04 +0300

    Create home page

--- JS WEB/NodeServerPapazov/index.js
+++ JS WEB/NodeServerPapazov/index.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const catalog = require('./views/catalog');
+const about = require('./views/about');
+const home = require('./views/home');
 const port = 3000;
 
 function requestHandler(req, res){
     if(req.url === '/'){
-        res.write('Hello Node!');
+        res.write(home);
     }else if(req.url == '/catalog'){
         res.write(catalog);
     }else if(req.url == '/about'){
-        res.write('About Page!');
+        res.write(about);
     }
     
     res.end();
---
+++ JS WEB/NodeServerPapazov/views/about.js
@@ -0,0 +1,1 @@
+module.exports = `<h1>About Page!</h1>`
