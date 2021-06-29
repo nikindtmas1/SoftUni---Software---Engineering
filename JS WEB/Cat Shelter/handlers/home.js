2ffaa62f67133b083063c56d3c63391279835682
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-29 22:20:39 +0300

    Added code home.js

--- JS WEB/Cat Shelter/handlers/home.js
+++ JS WEB/Cat Shelter/handlers/home.js
@@ -3,3 +3,21 @@ const fs = require('fs');
 const path = require('path');
 const cats = require('../data/cats.json');
 
+module.exports = (req,res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if(pathname == '/' && req.method == 'GET'){
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        fs.readFile('../views/home/index.html', (err, data) => {
+            if(err){
+                console.log('Sam error');
+            }
+            res.write(data);
+            res.end();
+        });
+    }else{
+        return true;
+    }
+}
