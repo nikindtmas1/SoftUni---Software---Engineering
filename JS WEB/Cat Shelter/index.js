c62320055aafba86fdf7e4a78a4d26fed8215907
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-29 22:21:06 +0300

    Added code index.js

--- JS WEB/Cat Shelter/index.js
+++ JS WEB/Cat Shelter/index.js
@@ -1,10 +1,14 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers')
 
 http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.write('Hello JS World!');
-    res.end();
-}).listen(3000, () => console.log(`Server listen on port ${port}...`));
+    for(let handler of handlers){
+        if(!handler(req,res)){
+            break;
+        }
+    }
+}).listen(port, () => console.log(`Server listen on port ${port}...`));
