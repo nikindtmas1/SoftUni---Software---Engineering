e9ab465dba3e7753da506da85f2a8c72fb029b92
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-06-30 21:47:44 +0300

    Change index code

--- JS WEB/Cat Shelter/index.js
+++ JS WEB/Cat Shelter/index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const port = 3000;
-const handlers = require('./handlers')
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/plain'
+    // });
     for(let handler of handlers){
         if(!handler(req,res)){
             break;
