d4714e04e6fa346de9417fc311057922ad86bd37
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-04 18:53:36 +0300

    Added logic

--- JS WEB/Cat Shelter/handlers/cat.js
+++ JS WEB/Cat Shelter/handlers/cat.js
@@ -11,6 +11,13 @@ module.exports = (req, res) => {
 
     if(pathname === '/cats/add-cat' && req.method === 'GET'){
 
+        let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
+
+        const items = fs.createReadStream(filePath);
+
+        items.map('data', (data) => {
+            res.write(data);
+        })
 
     }else if(pathname === '/cats/add-breed' && req.method === 'GET'){
 
