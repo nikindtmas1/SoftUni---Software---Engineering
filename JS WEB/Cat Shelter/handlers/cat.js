fe262233a701bd79ae55ae8e654e09e9632fca9c
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-04 18:19:58 +0300

    Added modules cat.js

--- JS WEB/Cat Shelter/handlers/cat.js
+++ JS WEB/Cat Shelter/handlers/cat.js
@@ -2,3 +2,6 @@ const url = require('url');
 const path = require('path');
 const fs = require('fs');
 const qs = require('querystring');
+const formidable = require('formidable');
+const breeds = require('../data/breeds');
+const cats = require('../data/cats');
