e9efc7e0ed0e4f11cec758ae928fdafe4d75c588
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-04 17:19:10 +0300

    Added logic

--- JS WEB/Cat Shelter/handlers/static-files.js
+++ JS WEB/Cat Shelter/handlers/static-files.js
@@ -1,3 +1,18 @@
-function getContentType(){
-    
+function getContentType(url){
+    if(url.endsWith('css')){
+        return 'text/css';
+
+    }else if('htm'){
+        return 'text/html';
+
+    }else if('png'){
+        return 'text/png';
+
+    }else if('json'){
+        return 'text/json';
+
+    }else if('js'){
+        return 'text/js';
+    }
 }
+
