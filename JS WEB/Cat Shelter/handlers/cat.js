ee5cf5e0c5303c2a295dc270dd8b36e189c0d3d5
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-04 18:35:30 +0300

    Added logic cat.js

--- JS WEB/Cat Shelter/handlers/cat.js
+++ JS WEB/Cat Shelter/handlers/cat.js
@@ -5,3 +5,17 @@ const qs = require('querystring');
 const formidable = require('formidable');
 const breeds = require('../data/breeds');
 const cats = require('../data/cats');
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if(pathname === '/cats/add-cat' && req.method === 'GET'){
+
+
+    }else if(pathname === '/cats/add-breed' && req.method === 'GET'){
+
+
+    }else{
+        return true;
+    }
+}
