3a16d0ed982c5ec0a307dcdaa572102c9276cf99
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-05 20:39:36 +0300

    uncoment files

--- JS WEB/Cat Shelter/index.js
+++ JS WEB/Cat Shelter/index.js
@@ -3,9 +3,9 @@ const port = 3000;
 const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // });
     for(let handler of handlers){
 
         if(!handler(req,res)){
