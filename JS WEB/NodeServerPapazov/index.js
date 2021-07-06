c667cc0f552ac9df48821db5b08c12d32d77bba2
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-06 20:46:56 +0300

    Muve onCatRequire

--- JS WEB/NodeServerPapazov/index.js
+++ JS WEB/NodeServerPapazov/index.js
@@ -13,17 +13,7 @@ const home = require('./views/home');
 const port = 3000;
 
 const names = [];
-const onCatsRequest = (name) => {
 
-    if(names.includes(name)){
-
-        console.log(`Hello ${name} again!`);
-    }else{
-
-        console.log(`We have new cat - ${name}`);
-        names.push(name);
-    }
-}
 
 function requestHandler(req, res){
 
--- JS WEB/NodeServerPapazov/init.js
+++ JS WEB/NodeServerPapazov/init.js
@@ -1,2 +1,13 @@
 const pubSub = require('./pubSub');
 
+const onCatsRequest = (name) => {
+
+    if(names.includes(name)){
+
+        console.log(`Hello ${name} again!`);
+    }else{
+
+        console.log(`We have new cat - ${name}`);
+        names.push(name);
+    }
+}
