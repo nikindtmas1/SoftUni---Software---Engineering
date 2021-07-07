c2082536ee8985eb13ad27ba6fe38e739d75e840
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-07 22:11:38 +0300

    Create indexPubSub.js

--- JS WEB/NodeServerPapazov/indexPubSub.js
+++ JS WEB/NodeServerPapazov/indexPubSub.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
 const port = 5000;
 
 const reqHendlar = (req, res) => {
-    res.write('Hello Port');
+    let reqUrl = req.url;
+    let pathname = url.parse(reqUrl).pathname;
+    let params = querystring.parse(reqUrl.query);
+    
+    if(pathname === '/cats'){
+        
+        console.log(params.name);
+    res.write(`Hello ${pathname}`);
     res.end();
+    
+    }
 }
 
 const app = http.createServer(reqHendlar);
