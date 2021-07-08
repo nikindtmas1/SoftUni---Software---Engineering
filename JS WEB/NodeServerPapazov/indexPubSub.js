2c2433e26d808f960a6f1f75adf83a82ed8b8ab0
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-08 21:12:59 +0300

    Create cats.html

--- JS WEB/NodeServerPapazov/indexPubSub.js
+++ JS WEB/NodeServerPapazov/indexPubSub.js
@@ -1,19 +1,30 @@
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
+const fs = require('fs');
+
 
 const port = 5000;
 
 const reqHendlar = (req, res) => {
     let reqUrl = req.url;
-    let pathname = url.parse(reqUrl).pathname;
+    let path = url.parse(reqUrl).pathname;
     let params = querystring.parse(reqUrl.query);
+
+    console.log(path);
+    console.log(params);
     
-    if(pathname === '/cats'){
+    if(path === '/cats'){
         
-        console.log(params.name);
-    res.write(`Hello ${pathname}`);
-    res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./views/cats.html', (err, data) => {
+            if(err){
+                console.log('Sam Err');
+            }
+
+            res.write(data);
+            res.end();
+        })
     
     }
 }
