cdd0a7be20d1dad6d2777b1bb1baae277ca205dd
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-10 22:03:50 +0300

    Create switch

--- JS WEB/NodeServerPapazov/indexPubSub.js
+++ JS WEB/NodeServerPapazov/indexPubSub.js
@@ -13,30 +13,43 @@ const reqHendlar = (req, res) => {
 
     console.log(path);
     console.log(params);
-    
-    if(path === '/cats'){
+
+    switch(path){
+        case '/cats':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+                fs.readFile('./views/cats.html', (err, data) => {
+                    if(err){
+                        console.log('Sam Err');
+                    }
         
-    res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('./views/cats.html', (err, data) => {
-            if(err){
-                console.log('Sam Err');
-            }
-
-            res.write(data);
-            res.end();
-        });
-    
-    }else if(path === '/dogs'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('./views/dogs.html', (err, data) => {
-            if(err){
-                console.log('Sam Err');
-            }
-
-            res.write(data);
-            res.end();
-        });
+                    res.write(data);
+                    res.end();
+                });
     }
+    
+    // if(path === '/cats'){
+        
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    //     fs.readFile('./views/cats.html', (err, data) => {
+    //         if(err){
+    //             console.log('Sam Err');
+    //         }
+
+    //         res.write(data);
+    //         res.end();
+    //     });
+    
+    // }else if(path === '/dogs'){
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     fs.readFile('./views/dogs.html', (err, data) => {
+    //         if(err){
+    //             console.log('Sam Err');
+    //         }
+
+    //         res.write(data);
+    //         res.end();
+    //     });
+    // }
 }
 
 const app = http.createServer(reqHendlar);
