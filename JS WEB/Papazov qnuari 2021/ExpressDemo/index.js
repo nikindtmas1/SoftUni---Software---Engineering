abedb691ce675af6043b86e7b4b3526042884772
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-14 22:55:42 +0300

    Added code index.js

--- JS WEB/Papazov qnuari 2021/ExpressDemo/index.js
+++ JS WEB/Papazov qnuari 2021/ExpressDemo/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 const port = 3000;
 
+const homePage = require('./views/home');
+
+app.route('/home')
+.get((req, res) => {
+    res.send(homePage);
+})
+.post((req,res) => {
+    res.send('POST Home Page')
+})
+.put((req,res) => {
+    res.send('PUT Home Page')
+})
+
+
 app.get('/', (req, res) => {
+    res.status(200);
     res.send('Hello Express!');
 });
+
+app.get('/cats/:catsId', (req, res) => {
+const paramsObj = req.params;
+res.send(paramsObj.catsId)
+})
+
+app.post('/', (req, res) => {
+    res.send('POST request');
+})
 
 app.listen(port, () => console.log(`Server runing on port ${port}...`));
