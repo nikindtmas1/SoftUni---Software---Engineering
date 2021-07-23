f4c745361398f9b887144ee78c9f7904831b80d3
nikindtmas1 <niki_nik_74@abv.bg>
2021-07-23 11:12:35 +0300

    Require express.js in index.js

--- JS WEB/Papazov qnuari 2021/cubicle/index.js
+++ JS WEB/Papazov qnuari 2021/cubicle/index.js
@@ -3,16 +3,9 @@ const express = require('express');
 const app = express();
 console.log(process.env.NODE_ENV);
 const config = require('./config/config');
+const expressConfig = require('./config/express');
 
-const handlebars = require('express-handlebars');
-
-app.engine('hbs', handlebars({
-    extname:'hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-app.use(express.static('static'))
+expressConfig(app);
 
 app.get('/', (req, res) => {
     res.render('home', {layout: false});
