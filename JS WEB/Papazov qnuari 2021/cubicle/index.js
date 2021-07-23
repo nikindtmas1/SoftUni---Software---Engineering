56a6f381bf7850e39bd50f07a960462f7e19a266
nikindtmas1 <niki_nik_74@abv.bg>
2021-07-23 14:20:20 +0300

    Added code index.js

--- JS WEB/Papazov qnuari 2021/cubicle/index.js
+++ JS WEB/Papazov qnuari 2021/cubicle/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
+const routes = require('./routes');
 const app = express();
 console.log(process.env.NODE_ENV);
 const config = require('./config/config');
+
 const expressConfig = require('./config/express');
 
 expressConfig(app);
 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false});
-});
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server listening on port ${config.PORT}...`))
