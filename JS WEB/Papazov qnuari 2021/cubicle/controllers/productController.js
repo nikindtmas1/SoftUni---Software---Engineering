24a1a15fcbc740f5ee386faf929116634275724c
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-24 23:16:42 +0300

    Added controller

--- JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
+++ JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/',(req, res) => {
-    res.render('home', {layout: false});
+router.get('/', (req, res) => {
+    res.render('home', { layout: false });
 })
 
 router.get('/create', (req, res) => {
-        res.render('create', {layout: false});
-    });
+    res.render('create', { layout: false });
+});
 
+router.get('/details', (req, res) => {
+    res.render('datails', {layout: false});
+});
 
+router.get('/about', (req, res) => {
+    res.render('about', {layout: false});
+})
 module.exports = router;
