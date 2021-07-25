832ebd647d9274d21c8f4ac1091ea2d7e1c78c8b
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-07-25 17:50:26 +0300

    Added layout true

--- JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
+++ JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/create', (req, res) => {
-    res.render('create', { layout: false });
+    res.render('create');
 });
 
 router.get('/details', (req, res) => {
