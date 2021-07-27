2c0eb86547660581805fcd20fd5a2dd3109eada2
nikindtmas1 <niki_nik_74@abv.bg>
2021-07-27 14:40:19 +0300

    Added function getAll

--- JS WEB/Papazov qnuari 2021/cubicle/services/productService.js
+++ JS WEB/Papazov qnuari 2021/cubicle/services/productService.js
@@ -1,6 +1,11 @@
 const Cube = require('../models/cubic');
 const uniqid = require('uniqid');
 const fs = require('fs');
+const productsData = require('../config/products.json');
+
+function getAll(){
+    return productsData;
+}
 
 function createProduct(data){
     let cube = new Cube(
@@ -11,7 +16,14 @@ function createProduct(data){
         data.difficultyLevel
         );
 
-    fs.watchFile('../config/products.json');
+        productsData.push(cube);
+
+    fs.watchFile('../config/products.json', JSON.stringify(productsData), (err) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
 }
 
 module.exports = {
