bf5cff4a46b259629a3b41d9cbf4e21437b7ca84
nikindtmas1 <niki_nik_74@abv.bg>
2021-08-03 12:19:09 +0300

    Added code productController.js

--- JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
+++ JS WEB/Papazov qnuari 2021/cubicle/controllers/productController.js
@@ -4,7 +4,8 @@ const productService = require('../services/productService');
 
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Browse'});
+    let products = productService.getAll();
+    res.render('home', {title: 'Browse', products});
 })
 
 router.get('/create', (req, res) => {
