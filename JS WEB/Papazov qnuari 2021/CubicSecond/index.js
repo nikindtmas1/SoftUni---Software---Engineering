14e89e280d5aa127532614c8e4e755e4254c98f9
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-06 11:54:37 +0300

    Change code index.js

--- JS WEB/Papazov qnuari 2021/CubicSecond/index.js
+++ JS WEB/Papazov qnuari 2021/CubicSecond/index.js
@@ -4,7 +4,9 @@ const config = require('./config/config');
 //const port = 5000;
 
 app.get('/', (req, res) => {
+
     res.send('Hello Express!');
+    
 });
 
 app.listen(config.PORT, () => console.log(`Server running on port: ${config.PORT}...`));
