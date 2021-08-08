295e95db4440921343358b2c80fb2d2ab176447e
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-08-08 16:21:09 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/CubicSecond/index.js
+++ JS WEB/Papazov qnuari 2021/CubicSecond/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const config = require('./config/config');
 const expressSetup = require('./config/express');
-
+const routes = require('./routes');
 expressSetup(app);
 //const port = 5000;
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    res.send('Hello Express!');
+//     res.send('Hello Express!');
     
-});
+// });
+
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server running on port: ${config.PORT}...`));
