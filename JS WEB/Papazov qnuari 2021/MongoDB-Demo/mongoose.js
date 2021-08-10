eae6db19ef8f61505222636a5584bec23bc6cd82
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-10 13:41:36 +0300

    Require student

--- JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
+++ JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const connectStr = 'mongodb://localhost:27017/testdb';
+//const Student = require('./models/Students');
 
 mongoose.connect(connectStr, {useUnifiedTopology: true,  useNewUrlParser: true});
 
@@ -15,9 +16,22 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema);
 
-let person = new Person({name: 'Stamat', age: 25});
+let person = new Person({name: 'Stefan', age: 55});
 person.save((err, result) => {
     if (err) return console.log(err);
 
     console.log(result);
 });
+
+// let Student = new Student({
+//     firstName:'Nikolay',
+//     lastName: 'Nikolaew',
+//     facultyNumber: '123456',
+//     age: 47
+// });
+
+// student.save((err, result) => {
+//     if (err) return console.log(err);
+
+//     console.log(result);
+// })
