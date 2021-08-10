cd117ad297c59ca115acf3018a03739730259f9f
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-10 15:18:11 +0300

    Change code

--- JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
+++ JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectStr = 'mongodb://localhost:27017/testdb';
-//const Student = require('./models/Students');
+const Student = require('./models/Students');
 
 mongoose.connect(connectStr, {useUnifiedTopology: true,  useNewUrlParser: true});
 
@@ -9,29 +9,29 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=> {console.log('Connected to data base')});
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number
+// });
+
+// const Person = mongoose.model('Person', personSchema);
+
+// let person = new Person({name: 'Stefan', age: 55});
+// person.save((err, result) => {
+//     if (err) return console.log(err);
+
+//     console.log(result);
+// });
+
+let student = new Student({
+    firstName:'Nikolay',
+    lastName: 'Nikolaew',
+    facultyNumber: '123456',
+    age: 47
 });
 
-const Person = mongoose.model('Person', personSchema);
-
-let person = new Person({name: 'Stefan', age: 55});
-person.save((err, result) => {
+student.save((err, result) => {
     if (err) return console.log(err);
 
     console.log(result);
 });
-
-// let Student = new Student({
-//     firstName:'Nikolay',
-//     lastName: 'Nikolaew',
-//     facultyNumber: '123456',
-//     age: 47
-// });
-
-// student.save((err, result) => {
-//     if (err) return console.log(err);
-
-//     console.log(result);
-// })
