2d3ca6988f54948a424cc5d8741d107744d9093d
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-10 11:06:42 +0300

    Connect to data base

--- JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
+++ JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 const connectStr = 'mongodb://localhost:27017';
 
-mongoose.connect(connectStr, {});
+mongoose.connect(connectStr, {useUnifiedTopology: true,  useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=> {console.log('Connected to data base')});
