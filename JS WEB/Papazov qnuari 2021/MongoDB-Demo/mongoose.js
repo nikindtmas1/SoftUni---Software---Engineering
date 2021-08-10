d5019cd6a596aecf8f2b379ccf207df2d64d69a1
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-10 11:26:40 +0300

    Added person in data base

--- JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
+++ JS WEB/Papazov qnuari 2021/MongoDB-Demo/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectStr = 'mongodb://localhost:27017';
+const connectStr = 'mongodb://localhost:27017/testdb';
 
 mongoose.connect(connectStr, {useUnifiedTopology: true,  useNewUrlParser: true});
 
@@ -7,3 +7,17 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=> {console.log('Connected to data base')});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+let person = new Person({name: 'Stamat', age: 25});
+person.save((err, result) => {
+    if (err) return console.log(err);
+
+    console.log(result);
+});
