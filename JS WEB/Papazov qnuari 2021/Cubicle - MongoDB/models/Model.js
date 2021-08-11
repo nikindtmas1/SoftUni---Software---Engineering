7187301e62ec3ebfd221ee791659734f39c269e4
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-11 15:40:56 +0300

    Create Model.js

---
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/models/Model.js
@@ -0,0 +1,29 @@
+const fs = require('fs/promises');
+const productsDb = require('../config/products.json');
+const path = require('path');
+
+
+class Model{
+
+    save(){
+
+        productsDb.push(this);
+
+       return fs.writeFile(
+            path.join(__dirname, '../config/products.json'),
+            JSON.stringify(productsDb)
+        )
+    }
+
+    static getAll(){
+        return productsDb;
+    }
+
+    static getOne(id){
+      return  productsDb.find(x => x.id == id);
+    }
+    
+}
+
+
+module.exports = Model;
