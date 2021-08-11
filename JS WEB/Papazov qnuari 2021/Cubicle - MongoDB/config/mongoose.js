430b0f3e16ab54c3ea95cda3776910bf5a0c3f39
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-08-11 23:04:19 +0300

    config/mongoose.js

---
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = (app) => {
+    
+    mongoose.connect('mongodb://localhost:27017/cubicle', {useNewUrlParser: true, useUnifiedTopology: true});
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console,'connection error:'));
+    db.once('open', function(){
+        console.log('db connected');
+    })
+}
