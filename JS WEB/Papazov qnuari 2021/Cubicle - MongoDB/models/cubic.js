9a5e2e202501be105dc4ecf75b95b64b0f0d8ead
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-11 14:04:57 +0300

    Change code cubic.js

--- JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/models/cubic.js
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/models/cubic.js
@@ -1,4 +1,6 @@
-
+const fs = require('fs/promises');
+const productsDb = require('../config/products.json');
+const path = require('path');
 
 class Cube {
     constructor(id, name, description, imageUrl, level){
@@ -9,7 +11,23 @@ class Cube {
         this.level = level;
     }
 
-    
+    save(){
+
+        productsDb.push(this);
+
+       return fs.writeFile(
+            path.join(__dirname, '../config/products.json'),
+            JSON.stringify(productsDb)
+        )
+    }
+
+    static getAll(){
+        return productsDb;
+    }
+
+    static getOne(id){
+      return  productsDb.find(x => x.id == id);
+    }
 }
 
 module.exports = Cube;
