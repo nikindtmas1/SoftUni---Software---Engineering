94971470a1aeb4e04fa2312307125bf3e001c6f5
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-12 09:58:21 +0300

    Change cubic.js

--- JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/models/cubic.js
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/models/cubic.js
@@ -1,23 +1,35 @@
 const mongoose = require('mongoose');
+
+
 const cubeSchema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         required: true
     },
-    description: String,
-    imageUrl: String,
-    difficultyLevel: Number
+    description: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/,
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6,
+    },
+    accessories: [
+        {
+            type: mongoose.Types.ObjecId,
+            ref: 'Accessory',
+        }
+    ]
 })
 
-class Cube {
-    constructor(id, name, description, imageUrl, level){
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.level = level;
-    }
 
-}
 
-module.exports = Cube;
+module.exports = mongoose.model('Cube', cubeSchema);
