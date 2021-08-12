2bc4d3c3474e455f5f70b23d6d124027feac997c
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-12 14:35:51 +0300

    Added router

--- JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/routes.js
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const productController = require('./controllers/productController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 
 
 router.use('/products', productController);
+router.use('/accessory', accessoryController);
 router.use('/', homeController);
 router.get('*', (req, res) => {
     res.render('404');
