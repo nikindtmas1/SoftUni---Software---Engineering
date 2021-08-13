c185489b6a477b4ba662b29872cbc5a46a3aab82
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-13 09:45:13 +0300

    Create accessoryController.js

--- JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/controllers/accessoryController.js
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/controllers/accessoryController.js
@@ -4,6 +4,13 @@ const router = express.Router();
 router.get('/create', (req, res) => {
 
     res.render('createAccessory')
-})
+});
+
+router.post('/create', (req, res) =>{
+    let data = req.body
+    console.log(data);
+
+    res.redirect('/products')
+});
 
 module.exports = router;
