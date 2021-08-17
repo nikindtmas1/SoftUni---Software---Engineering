b2e95d0f111d66d8aceffd96db0f64d7b3ceb056
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-08-17 23:31:16 +0300

    Added code mongoose.js

--- JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/config/mongoose.js
+++ JS WEB/Papazov qnuari 2021/Cubicle - MongoDB/config/mongoose.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 module.exports = (app) => {
     
-    mongoose.connect('mongodb://localhost:27017/cubicle', {useNewUrlParser: true, useUnifiedTopology: true});
-    //mongoose.connect("mongodb://cluster0-shard-00-00.3bvrs.mongodb.net:27017,cluster0-shard-00-01.3bvrs.mongodb.net:27017,cluster0-shard-00-02.3bvrs.mongodb.net:27017/Cubics?replicaSet=atlas-tekw8p-shard-0 --ssl --authenticationDatabase admin --username nikindtmas --password niki548444", {useNewUrlParser: true, useUnifiedTopology: true});
-    //mongoose.connect("mongodb://cluster0-shard-00-00.3bvrs.mongodb.net:27017,cluster0-shard-00-01.3bvrs.mongodb.net:27017,cluster0-shard-00-02.3bvrs.mongodb.net:27017/Cubics?replicaSet=atlas-tekw8p-shard-0", { useNewUrlParser: true, useNewUrlParser: true, useUnifiedTopology: true })
+    //mongoose.connect('mongodb://localhost:27017/cubicle', {useNewUrlParser: true, useUnifiedTopology: true});
+    switch (process.env.NODE_ENV.trim()) {
+        case 'production':
+            mongoose.connect('mongodb+srv://cluster0.3bvrs.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, tls: true, tlsCertificateKeyFile: './X509-cert-5779710120689814657.pem'});
+        default:
+            mongoose.connect(`mongodb+srv://nikindtmas:niki548444@cluster0.3bvrs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {useUnifiedTopology: true, useNewUrlParser: true, auth: {user: 'nikindtmas', password: 'niki548444'}});
+    }
 
     const db = mongoose.connection;
     db.on('error', console.error.bind(console,'connection error:'));
