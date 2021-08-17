8bc18118be676ef41e7843bd9acdeacd0bab5716
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-08-17 21:20:31 +0300

    Added cookies

--- JS WEB/Papazov qnuari 2021/Authentication/index.js
+++ JS WEB/Papazov qnuari 2021/Authentication/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 5000;
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+    res.cookie('CUSTOM_COOKIE', 'Initial Cookie Value');
     res.send('Hello!')
+});
+
+app.get('/show', (req, res) => {
+    console.log(req.cookies);
 })
 
 app.listen(port, () => console.log(`Server running on port: ${port}...`))
