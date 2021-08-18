023c4fa09972f8e449dd892908e06a0e4747c76c
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-18 11:47:28 +0300

    Added session

--- JS WEB/Papazov qnuari 2021/Authentication/index.js
+++ JS WEB/Papazov qnuari 2021/Authentication/index.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const uniqId = require('uniqid');
+
 
 const app = express();
 const port = 5000;
 
+const sessionData = {};
+
+const session = function(){
+    return (req, res, next)=>{
+        if(!req.cookies.id){
+            let userId = uniqId();
+            sessionData[userId] = {};
+            res.cookie('id', userId);
+        }else{
+            let userId = req.cookies.id;
+            req.session = sessionData[userId]
+        }
+
+        next();
+    }
+}
+
 app.use(cookieParser());
+app.use(session());
 
 app.get('/', (req, res) => {
+    
     res.cookie('CUSTOM_COOKIE', 'Initial Cookie Value');
-    res.send('Hello!')
+    
+    res.send(`<h1>Hello ${req.session?.username || 'n\\a'}</h1>`)
 });
 
 app.get('/show', (req, res) => {
     console.log(req.cookies);
-})
+});
 
+app.get('/login/:username', (req, res) => {
+    req.session.username = req.params.username;
+    let username = req.params.username;
+    res.send(`Hi ${username}`);
+});
+
+app.get('/session', (req, res) => {
+    res.send(req.session);
+})
 app.listen(port, () => console.log(`Server running on port: ${port}...`))
