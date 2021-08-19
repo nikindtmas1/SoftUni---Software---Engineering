f8c26994906f68d620e96095628c53f5b57f13c7
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-19 15:26:05 +0300

    Change code

--- JS WEB/Papazov qnuari 2021/CubicThird/index.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const setupExpress = require('./config/configExpress');
 setupExpress(app);
+const routes = require('./routes');
 const port = 5000;
 
 
+app.use(routes);
 
+// app.get('/', (req,res) => {
 
-app.get('/', (req,res) => {
-
-    res.render('home.hbs');
-})
+//     res.render('home.hbs');
+// })
 
 app.listen(port, () => console.log(`Server running on port: ${port}...`));
