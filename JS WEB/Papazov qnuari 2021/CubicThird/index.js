364c81d5f08e613080233e4cbb4b5813a7fb9065
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-19 14:01:16 +0300

    Change code

--- JS WEB/Papazov qnuari 2021/CubicThird/index.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const exprhbs = require('express-handlebars')
+
 
 const app = express();
 const port = 5000;
 
+app.engine('handlebars', exprhbs());
+app.set('view engine', 'handlebars');
+
+
 app.get('/', (req,res) => {
 
-    res.send('Hello from express!!!');
+    res.render('home.handlebars');
 })
 
 app.listen(port, () => console.log(`Server running on port: ${port}...`));
