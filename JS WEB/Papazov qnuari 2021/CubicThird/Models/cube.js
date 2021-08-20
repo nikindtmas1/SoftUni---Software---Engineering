8951dbcfa3101961440a52c2a4740b07dc6e19b9
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-20 10:56:07 +0300

    Added code cube.js

--- JS WEB/Papazov qnuari 2021/CubicThird/Models/cube.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/Models/cube.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+
+const cubeSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/
+    },
+
+    difficultyLevel: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 6
+    }
+});
+
+module.exports = mongoose.model('cube', cubeSchema);
