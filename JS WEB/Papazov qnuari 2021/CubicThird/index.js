e66339b541d31cfba45baf9c1c7b3fd662aacf79
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-20 15:04:36 +0300

    Change code

--- JS WEB/Papazov qnuari 2021/CubicThird/index.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+console.log(process.env.NODE_ENV);
 const setupExpress = require('./config/configExpress');
 setupExpress(app);
 const routes = require('./routes');
