e70792275ca1dabd6c62d8eedc414f4216e4e1ff
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-24 09:32:02 +0300

    Required accessoryController

--- JS WEB/Papazov qnuari 2021/CubicThird/routes.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const productController = require('./controllers/productController')
+const productController = require('./controllers/productController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use('/', productController);
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {
     res.render('404.hbs');
 });
