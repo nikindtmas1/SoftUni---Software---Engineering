2a74208249b28b18c9bd43d765eec60dd5597e73
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-26 10:01:32 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/CubicThird/services/productService.js
+++ JS WEB/Papazov qnuari 2021/CubicThird/services/productService.js
@@ -5,6 +5,18 @@ async function getAll(query){
 
     let products = await Cube.find({}).lean();
 
+    if(query.search){
+        products = products.filter(x => x.name.toLowerCase().includes(query.search));
+    };
+
+    if(query.from){
+        products = products.filter(x => Number(x.level) >= query.from);
+    };
+
+    if(query.to){
+        products = products.filter(x => Number(x.level) <= query.to);
+    };
+
     return products;
 
 }
