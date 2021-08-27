83e27985d781a3d4b04c10ba4e5904d6cb24cf1a
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-27 13:16:47 +0300

    Create Body.js

---
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Body extends React.Component {
+
+    render() {
+        return (
+            <main>
+                <p>Lorem 20</p>
+            </main>
+        );
+    }
+}
+
+export default Body;
