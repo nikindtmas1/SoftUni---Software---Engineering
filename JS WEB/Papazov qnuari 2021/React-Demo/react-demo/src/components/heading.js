1ccc8cdeac3bbb89d87fb61a33fe13ea728db16f
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-27 10:50:37 +0300

    Create components/heading.js

---
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/heading.js
@@ -0,0 +1,11 @@
+function Heading(props){
+    return (
+        <header>
+            <h1>Hello World!!!</h1>
+            <h2>Rect is the best!!!</h2>
+        </header>
+    )
+
+}
+
+export default Heading;
