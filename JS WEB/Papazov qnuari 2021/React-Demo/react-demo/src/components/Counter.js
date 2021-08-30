b86ef0699485f0334f5ba97343cae8f3ec10c2bf
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-08-30 09:19:23 +0300

    Create counter code

--- JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Counter.js
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Counter.js
@@ -1,2 +1,20 @@
 import { Component} from "react";
 
+class Counter extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div className='Counter'>
+
+                <span>10</span>
+                <button>+</button>
+            </div>
+            )
+    }
+}
+
+export default Counter;
