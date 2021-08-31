bfb4c5a82ba3fc7727c4634175f0ff554257cbc1
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-08-31 22:06:09 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Book.js
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Book.js
@@ -4,8 +4,8 @@ function Book(props){
 
 
 return(
-    <article className={props.isSelected ? 'Selected-book' : ''}>
-        <h3>{props.title}</h3>
+    <article>
+        <h3 onClick={props.clickHandler}>{props.title}</h3>
         <p>{props.description || 'Default Description'}</p>
     </article>
 )
