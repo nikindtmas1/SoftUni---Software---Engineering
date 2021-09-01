e9ebc1838d20bf07f4d0a8ffe18b7c5677354720
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-01 10:03:26 +0300

    Import message

--- JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Footer.js
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Footer.js
@@ -1,9 +1,17 @@
 import { Component } from "react";
+import Message from './Message';
 
 class Footer extends Component {
 
     constructor(props){
         super(props)
+    }
+
+    render(){
+
+        return (
+            <Message text='All rights reserved' />
+        )
     }
 }
 
