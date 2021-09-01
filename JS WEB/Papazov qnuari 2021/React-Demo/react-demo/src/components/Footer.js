6e7120fc65d9daced8db00f0126ed2e0c1ea42f6
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-01 10:29:14 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Footer.js
+++ JS WEB/Papazov qnuari 2021/React-Demo/react-demo/src/components/Footer.js
@@ -5,11 +5,21 @@ class Footer extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+            showFooter: true
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({showFooter: false})
+        }, 2500)
     }
 
     render(){
 
-        return (
+        return this.state.showFooter && (
             <Message text='All rights reserved' />
         )
     }
