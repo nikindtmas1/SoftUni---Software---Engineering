f63575f9ae9dec5e609293e395c3e1867bdc521e
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-02 21:12:05 +0300

    Create Navigation.js

---
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Navigation.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Navigation extends Component{
+    render(){
+        return(
+            <ul className="Navigation">
+                <li>Going to 1</li>
+                <li>Going to 2</li>
+                <li>Going to 3</li>
+                <li>Going to 4</li>
+                <li>Going to 5</li>
+                <li>Going to 6</li>
+            </ul>
+        )
+    }
+}
+
+export default Navigation;
