2455e702526387abd85cf115c8ddae465117fbf7
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-02 22:48:41 +0300

    Added code

---
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+
+class Welcome extends Component{
+
+    render(){
+        return (
+            <h1 className='App-header'>Hello React from Home Component!!!</h1>
+        )
+    }
+}
+
+export default Welcome
