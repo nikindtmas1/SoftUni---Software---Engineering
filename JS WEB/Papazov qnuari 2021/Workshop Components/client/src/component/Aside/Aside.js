4ad82090c1b8b44073bb15ef40a7a649e75fdacb
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-03 10:42:12 +0300

    Change css

--- JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Aside/Aside.js
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Aside/Aside.js
@@ -10,14 +10,14 @@ class Aside extends Component{
             <aside className={styles.Aside}>
                 <ul>
                     <li className={styles.listItem}>Going to 1</li>
-                    <li>Going to 2</li>
-                    <li>Going to 3</li>
-                    <li>Going to 4</li>
-                    <li>Going to 5</li>
-                    <li>Going to 6</li>
-                    <li>Going to 7</li>
-                    <li>Going to 8</li>
-                    <li>Going to 9</li>
+                    <li className={styles.listItem}>Going to 2</li>
+                    <li className={styles.listItem}>Going to 3</li>
+                    <li className={styles.listItem}>Going to 4</li>
+                    <li className={styles.listItem}>Going to 5</li>
+                    <li className={styles.listItem}>Going to 6</li>
+                    <li className={styles.listItem}>Going to 7</li>
+                    <li className={styles.listItem}>Going to 8</li>
+                    <li className={styles.listItem}>Going to 9</li>
                 </ul>
             </aside>
         )
