799793295ac0837d693d5c9347c7150660e90d79
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-03 10:24:09 +0300

    Create Aside.js

---
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Aside/Aside.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+class Aside extends Component{
+
+    render(){
+
+        return (
+
+            <aside>
+                <ul>
+                    <li>Going to 1</li>
+                    <li>Going to 2</li>
+                    <li>Going to 3</li>
+                    <li>Going to 4</li>
+                    <li>Going to 5</li>
+                    <li>Going to 6</li>
+                    <li>Going to 7</li>
+                    <li>Going to 8</li>
+                    <li>Going to 9</li>
+                </ul>
+            </aside>
+        )
+    }
+}
+
+export default Aside;
