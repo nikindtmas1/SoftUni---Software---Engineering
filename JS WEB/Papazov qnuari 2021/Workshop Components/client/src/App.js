ba0fd721548ed9fde7c4026d732d3276f3e8ae88
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-03 10:17:37 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/Workshop Components/client/src/App.js
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Welcome from './component/Header/Header';
 import Navigation from './component/Navigation/Navigation';
