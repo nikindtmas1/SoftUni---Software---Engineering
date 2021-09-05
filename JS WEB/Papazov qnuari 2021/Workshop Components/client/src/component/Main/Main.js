e46cabffed0a89a88faa952862e4bc06389b04ec
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-05 22:19:33 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Main/Main.js
+++ JS WEB/Papazov qnuari 2021/Workshop Components/client/src/component/Main/Main.js
@@ -1,22 +1,37 @@
 import { Component } from "react";
 import styles from './Main.module.css';
 
-class Main extends Component{
+const Main = () => {
 
-    render(posts){
+    return (
 
-        return(
-          
-            <main className='Main'>
-                <h1 className={styles.h1}>Main Component</h1>
-               
-            </main>
-
+        <main className={styles.Main}>
             
-        )
-    }
+            <h1>Soooooome Heading</h1>
+            <p>Some posts</p>
+
+        </main>
+
+    )
 
 }
+
+// class Main extends Component{
+
+//     render(posts){
+
+//         return(
+          
+//             <main className='Main'>
+//                 <h1 className={styles.h1}>Main Component</h1>
+               
+//             </main>
+
+            
+//         )
+//     }
+
+// }
 
 export default Main;
 
