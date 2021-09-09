a237a7ec268625d4aa8144d0dadc75a53c0b62fe
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-09 14:04:08 +0300

    Added Header/Header.js

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/App.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header />
+
+      <Footer />
     </div>
   );
 }
--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Header/Header.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Header/Header.js
@@ -1,45 +1,35 @@
-<header id="site-header">
-    <nav class="navbar">
-        
-        <section class="navbar-dashboard">
-            <div class="first-bar">
-                <a href="#">Dashboard</a>
-                <a class="button" href="#">My Pets</a>
-                <a class="button" href="#">Add Pet</a>
-            </div>
-            <div class="second-bar">
-                <ul>
-                    <li>Welcome, {{username}}!</li>
-                    <li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                </ul>
-            </div>
-        </section>
-        <section class="navbar-anonymous">
-            <ul>
-                <li><a href="#"><i class="fas fa-user-plus"></i> Register</a></li>
-                <li><a href="#"><i class="fas fa-sign-in-alt"></i> Login</a></li>
-            </ul>
-        </section>
-    </nav>
-        </header>
 
-import styles from './Header.module.css';
+
+//import styles from './Header.module.css';
 
 const Header = () => {
 
     return (
         
-      <nav className={styles.navbar}>
-          <p className={styles.dashboard}>Dashboard</p>
-          <ul>
-              <li>
-                  <a>Register</a>
-                  <a>Login</a>
-              </li>
-          </ul>
-
-          
-      </nav>
+        <header id="site-header">
+        <nav class="navbar">
+            
+            <section class="navbar-dashboard">
+                <div class="first-bar">
+                    <a href="#">Dashboard</a>
+                    <a class="button" href="#">My Pets</a>
+                    <a class="button" href="#">Add Pet</a>
+                </div>
+                <div class="second-bar">
+                    <ul>
+                        <li>Welcome, {{username}}!</li>
+                        <li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                    </ul>
+                </div>
+            </section>
+            <section class="navbar-anonymous">
+                <ul>
+                    <li><a href="#"><i class="fas fa-user-plus"></i> Register</a></li>
+                    <li><a href="#"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+                </ul>
+            </section>
+        </nav>
+            </header>
            
             
        
