2e73c9ceb6e224bf0a6bacec70cdb25455e969fd
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-10 11:22:10 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Footer/Footer.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Footer/Footer.js
@@ -4,6 +4,21 @@ const Footer = () => {
 
         <footer id="site-footer">
             <p>@PetMyPet</p>
+
+            <style jsx>{`
+            footer {
+                margin-top: auto;
+                text-align: center;
+                background: #234465;
+            }
+            
+            footer p {
+                padding: 1rem 0;
+                font-weight: bold;
+                margin: 0rem;
+                color: white;
+            }
+            `}</style>
         </footer>
     )
 
