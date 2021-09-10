8255ad400e463cadcaa18e7ff0c8a1344caa5218
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-10 14:55:35 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/App.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import Create from './components/Create/Create';
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
+class App extends Component { 
+
+  render() {
+
+    return (
+      <div className="container">
+        <Header />
         <Main />
-      <Footer />
-    </div>
-  );
-}
+      
+        
+        <Footer />
+      </div>
+    );
 
+  }
+  
+  }
 export default App;
