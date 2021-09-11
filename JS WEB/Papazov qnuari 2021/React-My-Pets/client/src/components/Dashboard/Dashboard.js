f169b5e9388f7a949d13ce9d87e231bcf2802ad7
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-11 14:18:48 +0300

    Change code

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
@@ -55,6 +55,17 @@ const Dashboard = () => {
 
                 <style jsx>
                     {`
+
+.fa-heart {
+    color: red;
+}
+                    .detailsMyPet>p:nth-child(2),
+                    .deletePet>p:nth-child(2),
+                    .otherPet>p:nth-child(2),
+                    .myPet>p:nth-child(2) {
+                        font-size: 1.2rem;
+                        font-weight: bold;
+                    }
                     .dashboard>nav>ul {
                         display: flex;
                     
@@ -66,6 +77,49 @@ const Dashboard = () => {
                     
                     .dashboard .navbar {
                         justify-content: center;
+                    }
+
+                    .basic h1,
+                    .my-pets h1,
+                    .dashboard h1 {
+                        text-align: center;
+                        margin: 0rem;
+                        padding: 2rem 0 1rem 0;
+                    }
+                    .img {
+                        width: 15rem;
+                        position: relative;
+                        overflow: hidden;
+                        padding: 0rem;
+                    }
+                    
+                    img {
+                        display: block;
+                        position: absolute;
+                        left: 0;
+                        right: 0;
+                        width: 100%;
+                        height: auto;
+                        object-fit: cover;
+                    }
+                    
+                    .img::after {
+                        display: block;
+                        content: "";
+                        padding-top: 100%;
+                    }
+
+                    .button {
+                        display: block;
+                        padding: 0.6rem 1rem;
+                        border-radius: 0.4rem;
+                        text-decoration: none;
+                        font-weight: bold;
+                        background: cadetblue;
+                        color: rgb(255, 255, 255);
+                        border: none;
+                        font-size: 1rem;
+                    
                     }
                     `}
                 </style>
