9f41ef3bfc493f9265c2aa227271611377b125c3
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-11 12:23:44 +0300

    Change Main.js

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Main/Main.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Main/Main.js
@@ -22,6 +22,15 @@ const Main = () => {
 
             <style jsx>
                 {`
+
+                .basic {
+                    background: linear-gradient(rgba(250, 248, 248, 0.2), rgba(25, 167, 173, 0.2)), url(../img/We-need-to-talk-about-pets.jpg) no-repeat center;
+                    background-size: cover;
+                    height: 85vh;
+                    position: relative;
+                    overflow: hidden;
+                }
+
                 .basic h1,
                 .my-pets h1,
                 .dashboard h1 {
@@ -29,11 +38,11 @@ const Main = () => {
                     margin: 0rem;
                     padding: 2rem 0 1rem 0;
                 }
-                
+
                 h3 {
                     margin-bottom: 0;
                 }
-                
+
                 .pet-info {
                     padding-bottom: 1rem;
                 }
