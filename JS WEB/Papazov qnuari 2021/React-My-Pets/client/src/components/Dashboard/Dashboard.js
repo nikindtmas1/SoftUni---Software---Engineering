4bc37b0bb5f0d62d052d85453c56a1c74fea6514
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-13 10:47:48 +0300

    Change Dashboard.js

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import { Component } from 'react';
+import Pet from '../Pet/Pet';
 
-const Dashboard = () => {
+class Dashboard extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            pets: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:5000/pets')
+        .then(res => res.json())
+        .then(res => this.setState({pets: res}))
+        .catch(error => console.log(error))
+    }
+
+    render(){
     return (
 
         <section className="dashboard">
@@ -17,6 +35,8 @@ const Dashboard = () => {
                     </ul>
                 </nav>
                 <ul className="other-pets-list">
+
+                    {this.state.pets.map(x => <Pet key={x.id} name={x.name} />)}
                     <li className="otherPet">
                         <h3>Name: Gosho</h3>
                         <p>Category: Cat</p>
@@ -192,6 +212,7 @@ const Dashboard = () => {
             </section>
 
     )
+    }
 }
 
 export default Dashboard;
