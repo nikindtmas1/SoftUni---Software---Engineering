a6e409cd3fecb868fe5617daef8300353b70257f
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-13 09:06:27 +0300

    Added code

--- JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
+++ JS WEB/Papazov qnuari 2021/React-My-Pets/client/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-
+import { NavLink } from 'react-router-dom';
 
 const Dashboard = () => {
 
@@ -8,12 +8,12 @@ const Dashboard = () => {
                 <h1>Dashboard</h1>
                 <nav className="navbar">
                     <ul>
-                        <li><a href="#">All</a></li>
-                        <li><a href="#">Cats</a></li>
-                        <li><a href="#">Dogs</a></li>
-                        <li><a href="#">Parrots</a></li>
-                        <li><a href="#">Reptiles</a></li>
-                        <li><a href="#">Other</a></li>
+                        <li><NavLink activeClassName='nav-link-selected' to='/categories/all'>All</NavLink></li>
+                        <li><NavLink to='/categories/cats'>Cats</NavLink></li>
+                        <li><NavLink to='/categories/dogs'>Dogs</NavLink></li>
+                        <li><NavLink to='/categories/parrots'>Parrots</NavLink></li>
+                        <li><NavLink to='/categories/reptiles'>Reptiles</NavLink></li>
+                        <li><NavLink to='/categories/other'>Other</NavLink></li>
                     </ul>
                 </nav>
                 <ul className="other-pets-list">
