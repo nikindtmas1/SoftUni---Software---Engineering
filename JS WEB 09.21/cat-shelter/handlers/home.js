227ea56cde8d4eaf9d92514769bd5f8a491621f3
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-15 22:17:56 +0300

    Added code

---
+++ JS WEB 09.21/cat-shelter/handlers/home.js
@@ -0,0 +1,32 @@
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const cats = require('../data/cats.json');
+
+
+module.exports = (req, res) => {
+
+    const pathName = url.parse(req.url).pathName;
+
+    if(pathName === '/' && req.method === 'GET'){
+            // TO DO LOGIC
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/home/index.html')
+        );
+
+        fs.readFile(filePath, (error, data) => {
+            if(error){
+                console.log(error);
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+
+                res.write('file not found');
+                res.end();
+                return;
+            }
+        });
+    }else {
+        return true;
+    }
+}
