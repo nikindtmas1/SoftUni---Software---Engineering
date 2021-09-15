0cc16c1a0e90988f401efc3a144cdcebc86c999e
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-15 09:28:46 +0300

    Change code

--- JS WEB 09.21/Sreams/index.js
+++ JS WEB 09.21/Sreams/index.js
@@ -1,21 +1,21 @@
-//const http = require('http');
-const events = require('events');
+const http = require('http');
+// const events = require('events');
 
 
-let eventEmiter = new events.EventEmitter();
+// let eventEmiter = new events.EventEmitter();
 
-eventEmiter.on('click', (a, b) => {
-    console.log('A click has been detected!');
-    console.log(a + ' ' + b);
-});
+// eventEmiter.on('click', (a, b) => {
+//     console.log('A click has been detected!');
+//     console.log(a + ' ' + b);
+// });
 
-eventEmiter.emit('click', 'Hello', 'World!');
+// eventEmiter.emit('click', 'Hello', 'World!');
 
-// http.createServer((req, res) => {
+http.createServer((req, res) => {
 
-//     res.write('Hi !');
-//     res.end();
+    res.write('Hi !');
+    res.end();
 
  
  
-// }).listen(5000);
+}).listen(5000);
