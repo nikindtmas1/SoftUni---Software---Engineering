d570343e287bbe1402b085400f50bfe8a2ec93ff
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-15 22:47:14 +0300

    Added code

--- JS WEB 09.21/cat-shelter/index.js
+++ JS WEB 09.21/cat-shelter/index.js
@@ -1,56 +1,70 @@
 const http = require('http');
 //const url = require('url');
-const fs = require('fs');
+//const fs = require('fs');
+const handlers = require('./handlers');
+
+const port = 5000;
 
 const app = http.createServer((req, res) => {
+
+    for (let handler of handlers) {
+        if(!handler(req, res)){
+            break;
+        }
+        
+    }
     //et urlObj = url.parse(req.url);
     // console.log('hostname: ' + urlObj.hostname);
     // console.log('href: ' + urlObj.href);
     // console.log('path: ' + urlObj.path);
     // console.log('port: ' + urlObj.port);
     // console.log('query: ' + urlObj.query);
-    switch (req.url) {
-        case '/':
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            let html = fs.readFileSync('./views/home/index.html');
-            res.write(html);
-            break;
-        case'/content/styles/site.css':
-            res.writeHead(200, {
-                'Content-Type': 'text/css'
-            });
-            let contentCss = fs.readFileSync('./content/styles/site.css');
-            res.write(contentCss);
-        break;
 
-        case'/cats/add-breed':
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            let addBreed = fs.readFileSync('./views/addBreed.html');
-            res.write(addBreed);
-        break;
+    //--- switch ---
+    // switch (req.url) {
+    //     case '/':
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         });
+    //         let html = fs.readFileSync('./views/home/index.html');
+    //         res.write(html);
+    //         break;
+    //     case'/content/styles/site.css':
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/css'
+    //         });
+    //         let contentCss = fs.readFileSync('./content/styles/site.css');
+    //         res.write(contentCss);
+    //     break;
 
-        case'/cats/add-cat':
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            let addCat = fs.readFileSync('./views/addCat.html');
-            res.write(addCat);
-        break;
-        default:
-            res.statusCode = 404
-            break;
-    }
+    //     case'/cats/add-breed':
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         });
+    //         let addBreed = fs.readFileSync('./views/addBreed.html');
+    //         res.write(addBreed);
+    //     break;
+
+    //     case'/cats/add-cat':
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         let addCat = fs.readFileSync('./views/addCat.html');
+    //         res.write(addCat);
+    //     break;
+    //     default:
+    //         res.statusCode = 404
+    //         break;
+    // }
+    // res.end();
+    // ---switch ---
+
     // console.log(req.method);
     // res.writeHead(200,{
     //     'Content-Type': 'text/html',
     // });
     // let html = fs.readFileSync('./views/home/index.html')
     // res.write(html);
-    res.end();
 });
 
-app.listen(5000);
+app.listen(port);
