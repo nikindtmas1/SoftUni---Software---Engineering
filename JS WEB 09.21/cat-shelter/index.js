d7548ca24ccade84644e02efb8efedbac1b2cae5
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-15 12:25:43 +0300

    Added code css

--- JS WEB 09.21/cat-shelter/index.js
+++ JS WEB 09.21/cat-shelter/index.js
@@ -3,18 +3,37 @@ const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-    // let urlObj = url.parse(req.url);
+    //et urlObj = url.parse(req.url);
     // console.log('hostname: ' + urlObj.hostname);
     // console.log('href: ' + urlObj.href);
     // console.log('path: ' + urlObj.path);
     // console.log('port: ' + urlObj.port);
     // console.log('query: ' + urlObj.query);
-    console.log(req.method);
-    res.writeHead(200,{
-        'Content-Type': 'text/html'
-    });
-    let html = fs.readFileSync('./views/home/index.html')
-    res.write(html);
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+            });
+            let html = fs.readFileSync('./views/home/index.html')
+            res.write(html);
+            break;
+        case'/content/styles/site.css':
+            res.writeHead(200, {
+                'Content-Type': 'text/css',
+            });
+            let contentCss = fs.readFileSync('./content/styles/site.css')
+            res.write(contentCss);
+        break;
+        default:
+            res.statusCode = 404
+            break;
+    }
+    // console.log(req.method);
+    // res.writeHead(200,{
+    //     'Content-Type': 'text/html',
+    // });
+    // let html = fs.readFileSync('./views/home/index.html')
+    // res.write(html);
     res.end();
 });
 
