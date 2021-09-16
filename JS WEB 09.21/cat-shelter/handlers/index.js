9e042aaf15de336c5fd3fa7c204b1f3e1db748a7
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-16 13:22:40 +0300

    Change code

--- JS WEB 09.21/cat-shelter/handlers/index.js
+++ JS WEB 09.21/cat-shelter/handlers/index.js
@@ -2,4 +2,5 @@ const homeHandler = require('./home');
 const staticFiles = require('./static-handler');
 const catHandler = require('./cat');
 
+
 module.exports = [homeHandler, staticFiles, catHandler]
