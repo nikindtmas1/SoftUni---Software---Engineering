888a0bd0660ace22ef1ee8039ed91268a75632d3
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-16 11:10:40 +0300

    Create cat page

---
+++ JS WEB 09.21/cat-shelter/handlers/cat.js
@@ -0,0 +1,37 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+const formidable = require('formidable');
+const breeds = require('../data/breeds.json');
+const cats = require('../data/cats.json');
+
+
+module.exports = (req, res) => {
+
+    let urlObj = url.parse(req.url);
+
+    if(urlObj.pathname === '/cats/add-cat' && req.method === 'GET'){
+
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/addCat.html')
+        );
+
+        fs.readFile(filePath, (error, data) => {
+            if(error){
+                console.log(error);
+
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.write('File Not Found');
+                res.end();
+                return;
+            }
+
+            
+
+        });
+
+    }
+}
