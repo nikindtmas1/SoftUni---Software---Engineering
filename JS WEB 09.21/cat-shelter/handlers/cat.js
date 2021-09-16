54c633e85b071c3eb9a80baef4bb5cc926114894
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-16 15:41:57 +0300

    Added logic

--- JS WEB 09.21/cat-shelter/handlers/cat.js
+++ JS WEB 09.21/cat-shelter/handlers/cat.js
@@ -64,30 +64,73 @@ module.exports = (req, res) => {
         });
 
     }else if(urlObj.pathname === '/cats/add-breed' && req.method === 'POST'){
+        console.log(req.method);
+        let formData = '';
 
-        let filePath = path.normalize(
-            path.join(__dirname, '../views/addBreed.html')
-        );
+        req.on('data', (data) => {
+            formData += data;
+        });
 
-        fs.readFile(filePath, (error, data) => {
-            if(error){
-                console.log(error);
+        req.on('end', () => {
 
-                res.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-                res.write('File Not Found');
-                res.end();
+            console.log(formData);
+
+            let body = fs.parse(formData);
+            console.log(body);
+            fs.readFile('./data/breeds.json' , (error, data) => {
+                if(error){
+                    return error;
+                }
+            });
+
+            let breeds = JSON.parse(data);
+            breeds.push(body.breed);
+            let json = JSON.stringify(breeds);
+
+            fs.writeFile('/data/breeds.json', json, 'utf-8', () => {
+                console.log('The breed has been added');
+            });
+
+            res.writeHead(200, {
+                pathname: '/'
+            });
+            res.end();
+        })
+        // let filePath = path.normalize(
+        //     path.join(__dirname, '../views/addBreed.html')
+        // );
+
+        // fs.readFile(filePath, (error, data) => {
+        //     if(error){
+        //         console.log(error);
+
+        //         res.writeHead(404, {
+        //             'Content-Type': 'text/html'
+        //         });
+        //         res.write('File Not Found');
+        //         res.end();
+        //         return;
+        //     }
+
+        //     res.writeHead(200, {
+        //         'Content-Type': 'text/html'
+        //     })
+        //     res.write(data);
+        //     res.end();
+
+        // });
+
+    }else if(urlObj.pathname === '/cats/add-cat' && req.method === 'POST'){
+        let form = new formidable.IncomingForm();
+
+        form.parse(req, (err, fields, files) => {
+            if(err){
+                console.error(err.message);
                 return;
             }
 
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.write(data);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end();
-
         });
-
     }
 }
