ac4750c87b03ea6947adb1cc42ac658519ffbc52
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-19 17:04:12 +0300

    Change code

--- JS WEB 09.21/cat-shelter/handlers/static-handler.js
+++ JS WEB 09.21/cat-shelter/handlers/static-handler.js
@@ -22,33 +22,59 @@ module.exports = (req, res) => {
     let urlObj = url.parse(req.url);
     //console.log(urlObj.pathname);
     if(urlObj.pathname.startsWith('/content') && req.method === 'GET'){
-        // if(urlObj.pathname.endsWith('jpg') || urlObj.pathname.endsWith('png')){
+        if(urlObj.pathname.endsWith('jpg') || urlObj.pathname.endsWith('png') && req.method === 'GET'){
 
-            
-        // }
-
-        fs.readFile(`./${urlObj.pathname}`, 'utf-8', (error, data) => {
-            if(error){
-                console.log(error);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-
-                res.write('File Not Found');
+            fs.readFile(`./${urlObj.pathname}`, (error, data) => {
+                if(error){
+                    console.log(error);
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+    
+                    res.write('File Not Found');
+                    res.end();
+                    return;
+                }
+    
+                //console.log(urlObj.pathname);
+    
+                res.writeHead(200, 
+                    {
+                        
+                    });
+    
+                res.write(data);
                 res.end();
-                return;
-            }
+            });
+            
+        }else{
 
-            //console.log(urlObj.pathname);
+            fs.readFile(`./${urlObj.pathname}`, 'utf-8', (error, data) => {
+                if(error){
+                    console.log(error);
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+    
+                    res.write('File Not Found');
+                    res.end();
+                    return;
+                }
+    
+                //console.log(urlObj.pathname);
+    
+                res.writeHead(200, 
+                    {
+                        'Content-Type': getContentType(urlObj.pathname)
+                    });
+    
+                res.write(data);
+                res.end();
+            });
 
-            res.writeHead(200, 
-                {
-                    'Content-Type': getContentType(urlObj.pathname)
-                });
+        }
 
-            res.write(data);
-            res.end();
-        });
+        
 
     }else{
         return true;
