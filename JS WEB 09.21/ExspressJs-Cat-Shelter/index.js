4f2fa2be40aa59bb1bcc479c17d6811c09472bf8
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-22 21:13:15 +0300

    Added code

--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
 const port = 5000;
-const router = require('./routes/routes');
+const routes = require('./routes');
 
+const app = express();
 //const home = require('./views/home/index.html')
 app.use('/static', express.static('static'));
-app.use('/',router);
+
 
 
 
@@ -18,5 +18,7 @@ app.get('/add-breed', (req, res) => {
 
     res.sendFile(__dirname + '/views/addBreed.html')
 })
+
+app.use(routes);
 
 app.listen(port, () => console.log(`Server listening on port: ${port}...`))
--- JS WEB 09.21/ExspressJs-Cat-Shelter/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req,res)=> {
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/html'
-    // });
-   res.sendFile(__dirname + '/views/home/index.html')
-});
