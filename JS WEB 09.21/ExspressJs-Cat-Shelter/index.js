735e8202e6d3c40a03663c71a3c42fa9ba85e8ed
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-22 18:10:42 +0300

    Added files

--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//const home = require('./views/home/index.html')
+app.use('/static', express.static('public'))
+
 app.get('/', (req,res)=> {
-    res.send('Hello From Express!');
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // });
+   res.sendFile(__dirname + '/views/home/index.html')
 })
 
 app.listen(port, () => console.log(`Server listening on port: ${port}...`))
