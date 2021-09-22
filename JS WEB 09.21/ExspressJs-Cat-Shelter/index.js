dbcee712d0ba384a6f661a90e49de249ca52c7a8
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-22 20:48:38 +0300

    Change code

--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const router = require('./routes/routes');
 
 //const home = require('./views/home/index.html')
 app.use('/static', express.static('static'));
+app.use('/',router);
 
 
-app.get('/', (req,res)=> {
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/html'
-    // });
-   res.sendFile(__dirname + '/views/home/index.html')
-});
 
 app.get('/add-cat', (req, res) => {
 
