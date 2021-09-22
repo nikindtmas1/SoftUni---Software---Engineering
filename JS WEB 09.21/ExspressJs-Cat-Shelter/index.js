dddf5a241b801d579632285d7663b6d17fd04161
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-22 18:38:27 +0300

    Added route logic

--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -3,13 +3,24 @@ const app = express();
 const port = 5000;
 
 //const home = require('./views/home/index.html')
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static'));
+
 
 app.get('/', (req,res)=> {
     // res.writeHead(200, {
     //     'Content-Type': 'text/html'
     // });
    res.sendFile(__dirname + '/views/home/index.html')
+});
+
+app.get('/add-cat', (req, res) => {
+
+    res.sendFile(__dirname + '/views/addCat.html')
+});
+
+app.get('/add-breed', (req, res) => {
+
+    res.sendFile(__dirname + '/views/addBreed.html')
 })
 
 app.listen(port, () => console.log(`Server listening on port: ${port}...`))
