3f17c6556b639d286f326621bed9774f20306ad2
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-22 21:13:34 +0300

    Added code

---
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const homeController = require('./controllers/homeController.js');
+
+router.use('/', homeController);
+
+module.exports = router
