4f40bfc3dc9127decf6c02a8543f07c7b46c4e70
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-23 22:01:40 +0300

    Added logic

--- JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/addCatController.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/addCatController.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const router = express.Router();
 
 
 router.get('/add-cat', (req, res) => {
 
-    res.render('addCat', {layout: false});
+    res.render('addCat');
 
 });
 
--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
 const port = 5000;
 const routes = require('./routes');
 
