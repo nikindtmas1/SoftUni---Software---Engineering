a5eb920e392359a886d578fae92e196a06ade87c
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-09-23 21:09:06 +0300

    Added logic in routes.js

--- JS WEB 09.21/ExspressJs-Cat-Shelter/routes.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('./controllers/homeController');
+const addCatController = require('./controllers/addCatController');
 
 router.use('/', homeController);
+router.use('/add-cat', addCatController);
 
 module.exports = router
