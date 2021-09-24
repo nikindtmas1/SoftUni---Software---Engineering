784b098ea9e86014225c276a720136c1cd7fbd8a
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-24 11:15:02 +0300

    Added logic

--- JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/homeController.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/homeController.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 router.get('/', (req, res) => {
 
-    let filePath = path.normalize(
-        path.join(__dirname, '../views/home/index.html')
-        )
+    // let filePath = path.normalize(
+    //     path.join(__dirname, '../views/home/index.html')
+    //     )
 
-    fs.readFile(filePath, (error, data) => {
-        if(error){
-            res.writeHead(404, {
-                'Content-Type': 'text/html'
-            });
-            res.write('File Not Found');
-            res.end();
-            return;
-        }
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write(data);
-        res.end();
-    })
+    // fs.readFile(filePath, (error, data) => {
+    //     if(error){
+    //         res.writeHead(404, {
+    //             'Content-Type': 'text/html'
+    //         });
+    //         res.write('File Not Found');
+    //         res.end();
+    //         return;
+    //     }
+    //     res.writeHead(200, {
+    //         'Content-Type': 'text/html'
+    //     });
+    //     res.write(data);
+    //     res.end();
+    // })
+    res.render('home');
    
 })
 
--- JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const exphbs = require('express-handlebars');
 const port = 5000;
-const routes = require('./routes');
+//const routes = require('./routes');
 const app = express();
 //const home = require('./views/home/index.html')
 //app.use('/static', express.static('static'));
@@ -10,12 +10,12 @@ const expressConfig = require('./config/configExpress');
 
 expressConfig(app);
 
-// app.get('/', (req,res)=> {
-//     // res.writeHead(200, {
-//     //     'Content-Type': 'text/html'
-//     // });
-//    res.sendFile(__dirname + '/views/home/home.html')
-// });
+app.get('/', (req,res)=> {
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // });
+   res.render('home')
+});
 
 // app.engine('handlebars', exphbs());
 // app.set('view engine', 'handlebars');
@@ -27,9 +27,9 @@ app.get('/add-cat', (req, res) => {
 
 app.get('/add-breed', (req, res) => {
 
-    res.sendFile(__dirname + '/views/addBreed.html')
+    res.render('addBreed')
 })
 
-app.use(routes);
+//app.use(routes);
 
 app.listen(port, () => console.log(`Server listening on port: ${port}...`))
