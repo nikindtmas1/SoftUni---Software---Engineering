7a88257d2814db99a7afdb92f09f5309298fed5a
Nikolay Nikolaev <niki_nik_74@abv.bg>
2021-09-24 10:09:09 +0300

    Added logic

--- JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/homeController.js
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/controllers/homeController.js
@@ -3,29 +3,28 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-
 router.get('/', (req, res) => {
 
     let filePath = path.normalize(
         path.join(__dirname, '../views/home/index.html')
-    )
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+        )
 
     fs.readFile(filePath, (error, data) => {
         if(error){
             res.writeHead(404, {
                 'Content-Type': 'text/html'
             });
-            res.write('File not found');
+            res.write('File Not Found');
             res.end();
+            return;
         }
-
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
         res.write(data);
         res.end();
     })
-    
+   
 })
 
  module.exports = router;
