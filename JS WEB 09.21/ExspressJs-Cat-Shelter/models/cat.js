3095d413c6dc06aabb4c7be4ad32972b49e3ad97
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-28 11:59:33 +0300

    Create cat.js
    Create cat model

---
+++ JS WEB 09.21/ExspressJs-Cat-Shelter/models/cat.js
@@ -0,0 +1,11 @@
+
+class Cat{
+    constructor(catId, name, description, breed){
+        this.catId = catId,
+        this.name = name,
+        this.description = description,
+        this.breed = breed
+    }
+}
+
+module.exports = Cat;
