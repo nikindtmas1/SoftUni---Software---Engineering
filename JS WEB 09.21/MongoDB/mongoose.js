6774caf27f15cd57cd2eb4c34b60a718d668ea7c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-29 13:31:16 +0300

    Update mongoose.js
    Create new Person

--- JS WEB 09.21/MongoDB/mongoose.js
+++ JS WEB 09.21/MongoDB/mongoose.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
 
+const studentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    facultyNumber: String,
+    age: Number
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+const myPerson = new Student({
+    firstName: "Toma",
+    lastName: "Bikov",
+    facultyNumber: "56789",
+    age: 55
+})
+
 async function main(){
+    
     await mongoose.connect('mongodb://localhost:27017/testdb', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
 
     console.log('Data base connected');
+
+    await myPerson.save();
+
+    const data = await Student.find({});
+
+    console.log(data);
 };
 
-main();
+main()
