ac903acef330edce72737b04d206efc18a89156b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-04 10:10:42 +0300

    Create cookie

--- JS WEB 09.21/Authentication-Demo/index.js
+++ JS WEB 09.21/Authentication-Demo/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.cookie('CUSTOM-COOKIE', 'Initial cookie value')
     res.send('<h1>Hello Express!</h1>')
+});
+
+app.get('/show', (req, res) => {
+    console.log(req.cookies);
+
+    res.json(req.cookies);
 });
 
 app.listen(5000, () => console.log('Server listening on port 5000...'));
