4577f0b091a27f03b195dfc95b3b6bcf5dc15852
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-04 09:56:46 +0300

    Create server

--- JS WEB 09.21/Authentication-Demo/index.js
+++ JS WEB 09.21/Authentication-Demo/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+
+    res.send('<h1>Hello Express!</h1>')
+});
+
+app.listen(5000, () => console.log('Server listening on port 5000...'));
