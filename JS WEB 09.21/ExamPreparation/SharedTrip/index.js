f9811e27cbba283a997100dbfe64cbbd6fb49aeb
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-06 21:42:05 +0300

    Required routes

--- JS WEB 09.21/ExamPreparation/SharedTrip/index.js
+++ JS WEB 09.21/ExamPreparation/SharedTrip/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes');
+
 const portConfig = require('./config/portConfig');
 const expressSetup = require('./config/expressConfig');
 
 expressSetup(app);
 require('./config/mongooseSetup');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+
+app.use(routes);
+
 
 app.listen(portConfig.PORT, () => console.log(`Server listening on port: ${portConfig.PORT}...`));
