021bea358d9ddd557ecac799c5ce225dc49ff538
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-06 13:59:19 +0300

    Config express-handlebars and express

--- JS WEB 09.21/ExamPreparation/SharedTrip/config/expressConfig.js
+++ JS WEB 09.21/ExamPreparation/SharedTrip/config/expressConfig.js
@@ -1,2 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
+
+function setUpExpress(app){
+
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
+
+    app.set('view engine', 'hbs');
+
+    app.use(express.static('static'));
+
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
+}
+
+module.exports = setUpExpress;
--- JS WEB 09.21/ExamPreparation/SharedTrip/index.js
+++ JS WEB 09.21/ExamPreparation/SharedTrip/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 const portConfig = require('./config/portConfig');
+const expressSetup = require('./config/expressConfig');
+
+expressSetup(app);
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.render('home');
 });
 
 app.listen(portConfig.PORT, () => console.log(`Server listening on port: ${portConfig.PORT}...`));
