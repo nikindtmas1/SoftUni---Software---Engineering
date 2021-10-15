7b99bbf1306efcaa9b57929d6822521984e7e516
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-15 20:59:37 +0300

    Create index.js

--- JS WEB 09.21/Sceleton/src/index.js
+++ JS WEB 09.21/Sceleton/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const configExpress = require('./config/configExpress');
+configExpress(app);
 
-    res.send('Hello express!')
-})
+const routes = require('./routes/routes');
+
+routes(app);
 
 app.listen(5000, () => console.log('Server listening on port 5000...'))
