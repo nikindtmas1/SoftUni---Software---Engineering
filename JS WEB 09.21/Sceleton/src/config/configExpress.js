5a952c7c9a9a5de3240abde831a032204c815255
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-15 20:59:12 +0300

    Create config/configExpress.js

---
+++ JS WEB 09.21/Sceleton/src/config/configExpress.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+module.exports = (app) => {
+
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
+
+    app.set('view engine', 'hbs');
+
+    app.use(express.urlencoded({extended: true}));
+
+    app.set('views', path.join(__dirname, '/src/views'));
+}
