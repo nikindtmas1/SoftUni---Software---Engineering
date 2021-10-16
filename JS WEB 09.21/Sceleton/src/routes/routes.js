fce556adfedc45457d23acae2f5f07f5b91dc4c6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-16 21:27:27 +0300

    Added controller

--- JS WEB 09.21/Sceleton/src/routes/routes.js
+++ JS WEB 09.21/Sceleton/src/routes/routes.js
@@ -1,10 +1,12 @@
 const homeController = require('../controllers/homeCotroller');
 const authController = require('../controllers/authController');
+const productsController = require('../controllers/productsController');
 
 
 module.exports = (app) => {
 
     app.use('/', homeController);
     app.use('/auth', authController);
+    app.use('/products', productsController);
     
 }
