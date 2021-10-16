df10fb30310722ca4c911abf542770a2d217dca0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-16 18:43:20 +0300

    Added auth controller directory

--- JS WEB 09.21/Sceleton/src/routes/routes.js
+++ JS WEB 09.21/Sceleton/src/routes/routes.js
@@ -1,8 +1,10 @@
 const homeController = require('../controllers/homeCotroller');
+const authController = require('../controllers/authController');
 
 
 module.exports = (app) => {
 
     app.use('/', homeController);
+    app.use('/auth', authController);
     
 }
