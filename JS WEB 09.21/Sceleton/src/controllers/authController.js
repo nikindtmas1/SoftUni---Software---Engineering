e5dcbb5a9695292a916a9730563aba32325c18d0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-16 19:12:47 +0300

    Create user register post router

--- JS WEB 09.21/Sceleton/src/controllers/authController.js
+++ JS WEB 09.21/Sceleton/src/controllers/authController.js
@@ -9,4 +9,10 @@ router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
+router.post('/register', (req, res) => {
+
+    
+    res.redirect('user/login');
+});
+
 module.exports = router;
