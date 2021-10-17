da48449ded6d2cd365c65daff1858735964bd297
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-17 11:33:50 +0300

    Added login logic

--- JS WEB 09.21/Sceleton/src/services/userService.js
+++ JS WEB 09.21/Sceleton/src/services/userService.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 
 function createUser(data){
@@ -8,8 +9,19 @@ let user = new User(data)
 return user.save();
 }
 
+function loginUser(data){
+    let name = data.username;
+    let password = data.password;
+
+    return User.findByUsername(name)
+    .then(user => {
+        return Promise.all([bcrypt.compare(password, user.password), user]);
+    });
+}
+
 module.exports = {
 
     createUser,
+    loginUser,
 
 }
