5579b35a34978ab3f0d1bda1ad5fa70d6e9d5697
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-17 12:03:59 +0300

    Required secret

--- JS WEB 09.21/Sceleton/src/controllers/userController.js
+++ JS WEB 09.21/Sceleton/src/controllers/userController.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 const userService = require('../services/userService');
+const { createToken } = require('../utils/jwtUtils');
 
 router.get('/login', (req, res) => {
     res.render('user/login');
+});
+
+router.post('/login',async (req, res) => {
+
+    let data = req.body;
+
+    let user = await userService.loginUser(data);
+
+    if(!user){
+
+        return res.redirect('404');
+
+    }
+
+    let token = await createToken(user);
+
+    res.cookie('cookieToken', token, {
+        httpOnly: true
+    });
+
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
