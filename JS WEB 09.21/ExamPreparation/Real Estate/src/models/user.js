265dd2ffd9b8eb28a772f69ce53bf10b1a874023
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-17 23:08:02 +0300

    Added paths

--- JS WEB 09.21/ExamPreparation/Real Estate/src/models/user.js
+++ JS WEB 09.21/ExamPreparation/Real Estate/src/models/user.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         required: true
