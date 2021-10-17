122dc3194c66272ea00dcfd8e9697796371bc0ca
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-17 11:34:20 +0300

    Added bcrypt hash password

--- JS WEB 09.21/Sceleton/src/models/user.js
+++ JS WEB 09.21/Sceleton/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -10,6 +11,18 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 5
     }
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.hash(this.password, 10)
+    .then(hash => {
+        this.password = hash;
+        next();
+    });
+});
+
+userSchema.static('findByUsername', function(username){
+    return this.findOne({username});
 });
 
 module.exports = mongoose.model('User', userSchema);
