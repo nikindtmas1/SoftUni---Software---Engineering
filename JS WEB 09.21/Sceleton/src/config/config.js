b5bbf1c1727598c039aa7c4aa7897d89c8bbb8cf
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-17 12:03:39 +0300

    Create secret

--- JS WEB 09.21/Sceleton/src/config/config.js
+++ JS WEB 09.21/Sceleton/src/config/config.js
@@ -2,7 +2,8 @@
 module.exports = {
     development: {
         PORT: 5000,
-        dbUrl: 'mongodb://localhost:27017/test'
+        dbUrl: 'mongodb://localhost:27017/test',
+        secret: 'mnogoqkaparola'
     },
     production: {
         PORT: 80
