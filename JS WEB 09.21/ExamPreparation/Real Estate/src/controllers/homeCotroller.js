9ae3e9a0d9bce0e87c2b3bd52c9ae7ed6f2d7574
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-18 10:12:42 +0300

    Change logic home page

--- JS WEB 09.21/ExamPreparation/Real Estate/src/controllers/homeCotroller.js
+++ JS WEB 09.21/ExamPreparation/Real Estate/src/controllers/homeCotroller.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   
-    res.render('home');
+const productService = require('../services/productService');
+
+router.get('/',async (req, res) => {
+    let results = await productService.getAllProduct();
+    res.render('home', {results});
 });
 
 module.exports = router;
