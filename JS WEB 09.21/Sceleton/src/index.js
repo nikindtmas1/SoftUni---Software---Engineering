d752b38a4234823017d89b223e7e60d087c2b7cb
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-18 09:28:05 +0300

    Set app.use cookieParser

--- JS WEB 09.21/Sceleton/src/index.js
+++ JS WEB 09.21/Sceleton/src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 const portConfig = require('./config/config');
 const configExpress = require('./config/configExpress');
 const mongooseConfig = require('./config/configMongoose');
