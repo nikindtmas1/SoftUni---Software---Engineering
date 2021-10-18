8de1cbe2f09e221a6ebdb74283738d9419d823fc
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-18 15:26:49 +0300

    Added promise valid user

--- JS WEB 09.21/ExamPreparation/Real Estate/src/services/userService.js
+++ JS WEB 09.21/ExamPreparation/Real Estate/src/services/userService.js
@@ -12,11 +12,19 @@ return user.save();
 function loginUser(data){
     let name = data.username;
     let password = data.password;
-
+   
     return User.findByUsername(name)
     .then(user => {
         return Promise.all([bcrypt.compare(password, user.password), user]);
-    });
+    })
+    .then(([isValid, user]) => {
+        if(isValid){
+            return user
+        }else {
+            //throw { message: 'Username or password are invalid'}
+            return null;
+        }
+    })
 }
 
 module.exports = {
