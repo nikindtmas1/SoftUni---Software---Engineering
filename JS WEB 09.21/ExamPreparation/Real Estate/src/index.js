9f93a7a4d9de06905c36f067df8075da0c739050
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-18 15:28:01 +0300

    Change position cinfigexpress

--- JS WEB 09.21/ExamPreparation/Real Estate/src/index.js
+++ JS WEB 09.21/ExamPreparation/Real Estate/src/index.js
@@ -8,13 +8,13 @@ app.use(cookieParser());
 const portConfig = require('./config/config');
 const configExpress = require('./config/configExpress');
 const mongooseConfig = require('./config/configMongoose');
-configExpress(app);
 
 const routes = require('./routes/routes');
 
 const { auth } = require('./middleware/authMiddleware');
 app.use(auth);
 
+configExpress(app);
 mongooseConfig(app)
 routes(app);
 
