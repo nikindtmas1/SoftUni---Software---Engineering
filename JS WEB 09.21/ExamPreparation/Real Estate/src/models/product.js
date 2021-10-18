dcafdaa0067c9a885d5b897a1ee2d1a122e43d45
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-18 10:05:12 +0300

    Added model product

--- JS WEB 09.21/ExamPreparation/Real Estate/src/models/product.js
+++ JS WEB 09.21/ExamPreparation/Real Estate/src/models/product.js
@@ -5,13 +5,39 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    description: {
+    type: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    city: {
         type: String,
         required: true
     },
     imageUrl: {
         type: String,
         required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    available: {
+        type: Number,
+        required: true
+    },
+    rented: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
     }
 });
 
