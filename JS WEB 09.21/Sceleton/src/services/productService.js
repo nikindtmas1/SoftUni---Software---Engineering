af294b907871c8acb838ac37c2efcac0d571d1ed
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-19 15:38:38 +0300

    Added productService.js

--- JS WEB 09.21/Sceleton/src/services/productService.js
+++ JS WEB 09.21/Sceleton/src/services/productService.js
@@ -1,9 +1,12 @@
 const Product = require('../models/product');
+const User = require('../models/user');
 
 
 function createProduct(data, userId){
-    let {name, description, imageUrl} = data;
-    let product = new Product({name, description, imageUrl, userId});
+
+    let {name, type, year, city, imageUrl, description, available, rented} = data
+
+    let product = new Product({name, type, year, city, imageUrl, description, available, rented, userId});
 
   return  product.save();
 
@@ -21,11 +24,37 @@ async function getOne(id){
   let results = await Product.findById(id).lean();
 
   return results;
-}
+};
+
+function deleteProduct(id){
+   
+  //return Product.deleteOne(result);
+  return Product.findByIdAndDelete(id);
+};
+
+async function updateOne(id, data){
+
+  let results = await Product.findByIdAndUpdate(id, data).lean();
+
+  return results;
+};
+
+async function rentProduct(prodId, userId){
+
+  let product = await Product.findById(prodId);
+  let user = await User.findById(userId);
+
+  product.rented.push(user);
+
+  return product.save();
+};
 
 module.exports = {
 
     createProduct,
     getAllProduct,
     getOne,
+    deleteProduct,
+    updateOne,
+    rentProduct,
 }
