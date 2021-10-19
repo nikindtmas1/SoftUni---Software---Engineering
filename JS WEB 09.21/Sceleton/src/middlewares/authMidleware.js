bc5223b608e9541d070a87b1eeb2236d53fec509
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-19 15:39:21 +0300

    Added authMidleware.js

--- JS WEB 09.21/Sceleton/src/middlewares/authMidleware.js
+++ JS WEB 09.21/Sceleton/src/middlewares/authMidleware.js
@@ -1,34 +1,36 @@
-const { development } = require('../config/config');
-const secretStr = development.secret;
+const  { development } = require('../config/config');
+//const secretStr = development.secret;
 const jwt = require('jsonwebtoken');
 
 
 function auth(req, res, next){
+
     let token = req.cookies.cookieToken;
 
     if(!token){
         return next();
     }
 
-    jwt.verify(token, secretStr, function(err, decodedToken){
-        if(err) {
+    jwt.verify(token, development.secret, function(err, decodedToken){
+
+        if(err){
             return res.status(401).redirect('/user/login');
         }
 
         req.user = decodedToken;
         res.locals.user = decodedToken;
         next();
-    });
 
+    });
 };
 
 function isAuth(req, res, next){
     if(!req.user){
-        return res.status(401).redirect('/auth/login');
-    }
+        return res.status(401).redirect('/user/login');
+    };
 
     next();
-};
+}
 
 module.exports = {
     auth,
