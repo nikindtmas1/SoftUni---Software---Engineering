a9fa68b3153225e43414d69359186af41bfbd048
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-19 15:40:00 +0300

    Added productsController.js

--- JS WEB 09.21/Sceleton/src/controllers/productsController.js
+++ JS WEB 09.21/Sceleton/src/controllers/productsController.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productService = require('../services/productService');
 
+const { isAuth } = require('../middleware/authMiddleware');
+const { isOwn } = require('../middleware/productMiddleware');
+
 
 router.get('/create', (req, res) => {
 
@@ -27,10 +30,62 @@ router.get('/show',async (req, res) => {
 router.get('/deatail/:prodId',async (req, res) => {
 
     let result = await productService.getOne(req.params.prodId);
+    let allProducts = await productService.getAllProduct();
+    let count = allProducts.length;
+    
+    
+    if(req.user){
+        let isOwn = result.userId == req.user._id;
+        let isAuth = req.user;
+        let userRented = result.rented.find((x) => x == req.user._id);
+       
+    
+        res.render('products/details', {result, isOwn, isAuth, count, userRented});
 
-    let isOwn = result.userId == req.user._id;
+    }else{
 
-    res.render('products/details', {result, isOwn});
+        res.render('products/details', {result});
+    }
+});
+
+router.get('/:prodId/rent',  async (req, res) => {
+
+    let allProducts = await productService.getAllProduct();
+    let count = allProducts.length;
+    count = count - 1;
+
+    productService.rentProduct(req.params.prodId, req.user._id)
+    .then(() => res.redirect(`/products/details/${req.params.prodId}`));
+    
+});
+
+router.get('/:prodId/delete', isAuth, isOwn,async (req, res) => {
+
+    //let result = await productService.getOne(req.params.prodId);
+
+    if(!req.user){
+        return res.redirect('/user/login');
+    };
+
+    await productService.deleteProduct(req.params.prodId);
+
+    res.redirect('/');
+});
+
+router.get('/:prodId/edit', isAuth, isOwn, async (req, res) => {
+    let result = await productService.getOne(req.params.prodId);
+
+    res.render('products/edit', {result});
+});
+
+router.post('/:prodId/edit', isAuth, isOwn, async (req, res) => {
+
+    let {name, type, year, city, imageUrl, description, available} = req.body;
+
+    await productService.updateOne(req.params.prodId, {name, type, year, city, imageUrl, description, available});
+
+    res.redirect(`/products/details/${req.params.prodId}`)
+
 });
 
 module.exports = router;
