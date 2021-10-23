7cefd888e60b7a237a544a1d33e3e77964c7b2a6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-23 18:39:32 +0300

    Added tryCatch

--- JS WEB 09.21/ExamPreparation/VideoTutorial/src/controllers/productsController.js
+++ JS WEB 09.21/ExamPreparation/VideoTutorial/src/controllers/productsController.js
@@ -26,45 +26,57 @@ router.post('/create',async (req, res) => {
 });
 
 router.get('/show',async (req, res) => {
-
-    let results = await productService.getAllProduct();
+    try {
+        let results = await productService.getAllProduct();
 
     res.render('products/show', {results});
+    } catch (error) {
+        console.log(error);
+        res.redirect('/products/show', {error: error.message});
+    }
+
+    
 });
 
 router.get('/details/:prodId',async (req, res) => {
 
-    let result = await productService.getOne(req.params.prodId);
-    //let allProducts = await productService.getAllProduct();
-    //let count = allProducts.length;
-   
+    try {
+        let result = await productService.getOne(req.params.prodId); 
+        if(req.user){
+            let isOwn = result.userId == req.user._id;
+            let isAuth = req.user;
+            let isEnroll = result.usersEnrolled.filter((x) => x == req.user._id)        //let userRented = result.rented.find((x) => x == req.user._id);
+            
+            
+            res.render('products/details', {result,isEnroll, isOwn, isAuth});//, count, userRented
     
-    if(req.user){
-        let isOwn = result.userId == req.user._id;
-        let isAuth = req.user;
-        let isEnroll = result.usersEnrolled.filter((x) => x == req.user._id)        //let userRented = result.rented.find((x) => x == req.user._id);
-        
-        
-        res.render('products/details', {result,isEnroll, isOwn, isAuth});//, count, userRented
-
-    }else{
-
-        res.render('products/details', {result});
+        }else{
+    
+            res.render('products/details', {result});
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect('products/details', {error: error.message});
     }
+   
 });
 
-router.get('/:prodId/enroll',  async (req, res) => {
+router.get('/:prodId/enroll', (req, res) => {
 
-
-    productService.enrollProduct(req.params.prodId, req.user._id)
-    .then(() => res.redirect(`/products/details/${req.params.prodId}`));
+    try {
+        productService.enrollProduct(req.params.prodId, req.user._id)
+        .then(() => res.redirect(`products/details/${req.params.prodId}`));
+    } catch (error) {
+        console.log(error);
+        res.redirect(`products/details/${req.params.prodId}`, {error: error.message});
+    }
+    
     
 });
 
 router.get('/:prodId/delete', isAuth, isOwn,async (req, res) => {
 
-   
-
+   try {
     if(!req.user){
         return res.redirect('/user/login');
     };
@@ -72,21 +84,37 @@ router.get('/:prodId/delete', isAuth, isOwn,async (req, res) => {
     await productService.deleteProduct(req.params.prodId);
 
     res.redirect('/');
+   } catch (error) {
+       console.log(error);
+       res.redirect('/user/login', {error: error.message});
+   }
+   
 });
 
 router.get('/:prodId/edit', isAuth, isOwn, async (req, res) => {
-    let result = await productService.getOne(req.params.prodId);
+    try {
+        let result = await productService.getOne(req.params.prodId);
 
     res.render('products/edit', {result});
+    } catch (error) {
+        console.log(error);
+        res.redirect('products/edit', {error: error.message});
+    }
+    
 });
 
 router.post('/:prodId/edit', isAuth, isOwn, async (req, res) => {
+    try {
+        let {title, imageUrl, description, isPublic} = req.body;
 
-    let {title, imageUrl, description, isPublic} = req.body;
-
-    await productService.updateOne(req.params.prodId, {title, imageUrl, description, isPublic});
-
-    res.redirect(`/products/details/${req.params.prodId}`)
+        await productService.updateOne(req.params.prodId, {title, imageUrl, description, isPublic});
+    
+        res.redirect(`/products/details/${req.params.prodId}`);
+    } catch (error) {
+        console.log(error);
+        res.redirect(`/products/details/${req.params.prodId}`, {error: error.message});
+    }
+   
 
 });
 
