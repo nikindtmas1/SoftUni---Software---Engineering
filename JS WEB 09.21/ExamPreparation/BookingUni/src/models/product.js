171298bf61f625b323675020fc0ee572a9b6edf4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-24 15:05:34 +0300

    Added array

--- JS WEB 09.21/ExamPreparation/BookingUni/src/models/product.js
+++ JS WEB 09.21/ExamPreparation/BookingUni/src/models/product.js
@@ -19,6 +19,12 @@ const productSchema = new mongoose.Schema({
         min: 1,
         max: 100
     },
+    usersBooked: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
     userId: {
         type: mongoose.Types.ObjectId,
         ref: 'User'
