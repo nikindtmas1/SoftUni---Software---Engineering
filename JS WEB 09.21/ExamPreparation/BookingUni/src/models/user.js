098f0a843dab5183be4f9e7177aacbf9c49169b4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-24 10:16:11 +0300

    Added module user

--- JS WEB 09.21/ExamPreparation/BookingUni/src/models/user.js
+++ JS WEB 09.21/ExamPreparation/BookingUni/src/models/user.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         required: true
