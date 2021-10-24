7ddb9e80d994cbffc8bef5e4147307a13d94c72b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-24 10:56:17 +0300

    Require productService

--- JS WEB 09.21/ExamPreparation/BookingUni/src/controllers/homeCotroller.js
+++ JS WEB 09.21/ExamPreparation/BookingUni/src/controllers/homeCotroller.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const productService = require('../services/productService');
+
 router.get('/', async (req, res) => {
 
     try {
-        //let results = await productService.getAllProduct();
+        let results = await productService.getAllProduct();
 
-        res.render('home');//, {results}
+        res.render('home', {results});//
 
     } catch (error) {
         console.log(error);
-        //res.redirect('/', {error: error.message});
+        res.redirect('/', {error: error.message});
     }
   
 });
