ec1b626e8bff84f80674a8dadbc64c892962cc4e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-26 10:37:21 +0300

    Added trycatch logic

--- JS WEB 09.21/Sceleton/src/controllers/productsController.js
+++ JS WEB 09.21/Sceleton/src/controllers/productsController.js
@@ -11,7 +11,7 @@ router.get('/create', (req, res) => {
     res.render('products/create');
 });
 
-router.post('/create',async (req, res) => {
+router.post('/create', async (req, res) => {
 
     try {
         let data = req.body;
@@ -19,100 +19,100 @@ router.post('/create',async (req, res) => {
         await productService.createProduct(data, req.user._id)
 
         res.redirect('/');
-        
+
     } catch (error) {
         console.log(error);
-        res.redirect('/', { error: error.message });
+        res.render('products/create', { error: error.message });
     }
-    
+
 });
 
-router.get('/show',async (req, res) => {
+router.get('/show', async (req, res) => {
     try {
-        
+
         let results = await productService.getAllProduct();
 
-    res.render('products/show', {results});
+        res.render('products/show', { results });
 
     } catch (error) {
         console.log(error);
-        res.redirect('/products/show', {error: error.message});
+        res.render('products/show', { error: error.message });
     }
 
-    
+
 });
 
-router.get('/details/:prodId',async (req, res) => {
+router.get('/details/:prodId', async (req, res) => {
 
     try {
         let result = await productService.getOne(req.params.prodId);
         let allProducts = await productService.getAllProduct();
         let count = allProducts.length;
-        
-        
-        if(req.user){
+
+
+        if (req.user) {
             let isOwn = result.userId == req.user._id;
             let isAuth = req.user;
             let userRented = result.rented.find((x) => x == req.user._id);
-           
-        
-            res.render('products/details', {result, isOwn, isAuth, count, userRented});
-    
-        }else{
-    
-            res.render('products/details', {result});
+
+
+            res.render('products/details', { result, isOwn, isAuth, count, userRented });
+
+        } else {
+
+            res.render('products/details', { result });
         }
 
     } catch (error) {
         console.log(error);
-        res.redirect('/details/:prodId', {error: error.message});
+        res.render('products/details', { error: error.message });
     }
-   
+
 });
 
-router.get('/:prodId/rent',  async (req, res) => {
+router.get('/:prodId/rent', async (req, res) => {
 
     try {
 
         let allProducts = await productService.getAllProduct();
-    let count = allProducts.length;
-    count = count - 1;
+        let count = allProducts.length;
+        count = count - 1;
 
-    productService.rentProduct(req.params.prodId, req.user._id)
-    .then(() => res.redirect(`/products/details/${req.params.prodId}`));
+        productService.rentProduct(req.params.prodId, req.user._id)
+            .then(() => res.redirect(`/products/details/${req.params.prodId}`));
 
     } catch (error) {
         console.log(error);
-        res.redirect('/:prodId/rent', {error: error.message});
+        res.render('products/details', { error: error.message });
     }
 
-    
-    
+
+
 });
 
 // router.get('/:prodId/delete', isAuth, isOwn,async (req, res) => {
 
 //     //let result = await productService.getOne(req.params.prodId);
 //     try {
-        
+
 //         if(!req.user){
 //             return res.redirect('/user/login');
 //         };
-    
+
 //         await productService.deleteProduct(req.params.prodId);
-    
+
 //         res.redirect('/');
 
 //     } catch (error) {
 //         console.log(error);
 //         res.redirect('/:prodId/delete', {error: error.message});
 //     }
-    
+
 // });
 
 // router.get('/:prodId/edit', isAuth, isOwn, async (req, res) => {
 //     try {
-        
+
 //         let result = await productService.getOne(req.params.prodId);
 
 //     res.render('products/edit', {result});
@@ -121,7 +121,7 @@ router.get('/:prodId/rent',  async (req, res) => {
 //         console.log(error);
 //         res.redirect('/:prodId/edit', {error: error.message});
 //     }
-    
+
 // });
 
 // router.post('/:prodId/edit', isAuth, isOwn, async (req, res) => {
