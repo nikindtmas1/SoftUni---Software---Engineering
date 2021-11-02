acd31a16e50f90294a13ee08fc31692f52ab6d60
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-02 13:26:37 +0200

    Added react demo

---
+++ ReactJS/reactDemo/react-demo/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  return (
+    <div className="App">
+      <h1>Helo From React</h1>
+    </div>
+  );
+}
+
+function Welcome(){
+  return (
+    <div>
+      <h1>Welcome from React</h1>
+    </div>
+  );
+}
+
+export default App;
+//export default Welcome;
