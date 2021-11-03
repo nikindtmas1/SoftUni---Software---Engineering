15ed8b6d2acfc5953d7991f834cc99719990a102
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-03 13:23:45 +0200

    Change code

--- ReactJS/toursProject/src/App.js
+++ ReactJS/toursProject/src/App.js
@@ -1,14 +1,24 @@
 
-//import './App.css';
+import './App.css';
 import Header from './components/Header.js';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer.js';
+import About from './components/About';
+import Service from './components/Service';
+import Agenci from './components/Agenci';
+import Text from './components/Text';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <main >
       <Navigation />
       <Header />
+      <About />
+      <Service />
+      <Agenci />
+      <Text />
+      <Contact />
       <Footer />
     </main>
   );
--- ReactJS/toursProject/src/components/Hello.js
+++
@@ -1,7 +0,0 @@
-export default function Hello(){
-    return (
-        <div>
-          <h1>Hello from React</h1>
-        </div>
-      );
-}
