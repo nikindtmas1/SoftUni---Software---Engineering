125bd7bfcec7766645b2985aee4d900586658de1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-03 11:13:56 +0200

    Added new project

---
+++ ReactJS/toursProject/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Hello from './components/Hello.js'
+import Welcome from './components/Welcome.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       <Hello />
+       <Welcome />
+        
+      </header>
+    </div>
+  );
+
+}
+
+
+
+export default App;
+//export default Welcome;
