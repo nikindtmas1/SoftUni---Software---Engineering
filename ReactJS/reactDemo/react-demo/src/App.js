c71b2eea1c53a5c5e701c49550a026c9e4ea5865
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-03 10:55:21 +0200

    Change code

--- ReactJS/reactDemo/react-demo/src/App.js
+++ ReactJS/reactDemo/react-demo/src/App.js
@@ -1,40 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Hello from './components/Hello.js'
+import Welcome from './components/Welcome.js'
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
   return (
     <div className="App">
-      <h1>Helo From React</h1>
+      <header className="App-header">
+        
+       <Hello />
+       <Welcome />
+        
+      </header>
     </div>
   );
+
 }
 
-function Welcome(){
-  return (
-    <div>
-      <h1>Welcome from React</h1>
-    </div>
-  );
-}
+
 
 export default App;
 //export default Welcome;
