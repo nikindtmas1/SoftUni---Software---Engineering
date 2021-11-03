62918829a8240d802584bb37f49e6ccd569249c2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-03 12:29:48 +0200

    Added components

---
+++ ReactJS/toursProject/src/components/Footer.js
@@ -0,0 +1,63 @@
+export default function Footer(){
+    return (
+    <footer>
+            <div class="container">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                </ul>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <div class="item">
+                            <h4 class="text-uppercase">Contact us</h4>
+                            <p class="address">
+                                123 Second Street Fifth <br />
+                                Avenue,<b />
+                                Manhattan, New York<br />
+                                +987 654 3210
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <div class="item">
+                            <h4 class="text-uppercase">additional links</h4>
+                            <ul>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Terms and conditions</a></li>
+                                <li><a href="#">Privacy policy</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <div class="item date">
+                            <h4 class="text-uppercase">resent posts</h4>
+                            <p><a href="#">01/03/2019</a></p>
+                            <p><a href="#">08/05/2019</a></p>
+                            <p><a href="#">01/03/2019</a></p>
+                            <p><a href="#">08/05/2019</a></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <div class="item">
+                            <h4 class="text-uppercase">newsletter</h4>
+                            <form>
+                                <input type="text" placeholder="Name" />
+                                <input type="email" placeholder="Email" />
+                                <input type="submit" value="Submit" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright text-center">
+                <p>Copyright 2019  Design by <a href="https://html.design">Free Html Templates</a></p>
+            </div>
+        </footer>
+        
+    );
+
+};
---
+++ ReactJS/toursProject/src/components/Navigation.js
@@ -0,0 +1,45 @@
+export default function Navigation(){
+
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light text-capitalize">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="imgs/logo.png" alt="#" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#show-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="show-menu">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">about</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#service">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#agency">travel agency</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">contact us</a>
+                        </li>
+                        <li class="nav-item .search-container">
+                            <a class="nav-link search" href="#"><i class="fas fa-search"></i></a>
+                            <form>
+                                <input type="search" />
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="far fa-user"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="far fa-heart"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
