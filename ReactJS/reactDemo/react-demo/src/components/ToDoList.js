4492f73ad17e4c7d14b1a290aed330e55f7c2b73
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-06 21:21:51 +0200

    Added class component

--- ReactJS/reactDemo/react-demo/src/components/ToDoList.js
+++ ReactJS/reactDemo/react-demo/src/components/ToDoList.js
@@ -10,3 +10,14 @@ const TodoList = () => {
 };
 
 export default TodoList;
+
+// import React from 'react';
+// class TodoList extends React.Component{
+//     render(){
+//         return(
+//             <ul>
+//                 <li>Clean your room</li>
+//             </ul>
+//         );
+//     };
+// };
