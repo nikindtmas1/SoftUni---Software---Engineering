65be87dad770c9b60c97115abf49ede793826df9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-06 21:16:48 +0200

    Added components

---
+++ ReactJS/reactDemo/react-demo/src/components/Books.js
@@ -0,0 +1,19 @@
+const BookList = () => {
+
+    return (
+        <ul>
+            <Book
+            title='IT'
+            author='Stephen King'
+            price='20'
+             />
+             <Book
+             title='The Hunger Games'
+             author='Suzzane Collins'
+             price='10'
+             />
+        </ul>
+    );
+};
+
+export default BookList;
---
+++ ReactJS/reactDemo/react-demo/src/components/ToDoList.js
@@ -0,0 +1,12 @@
+const TodoList = () => {
+
+    return (
+        <ul>
+            <li>Clean your room</li>
+            <li>Go shoping</li>
+            <li>Learn React</li>
+        </ul>
+    );
+};
+
+export default TodoList;
