6a049d58f2fe5fb76c9407e423fbfb88ecf5f3c7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-08 11:22:50 +0200

    Make change logic if/else

--- ReactJS/reactDemo/react-demo/src/components/TodoColorButton.js
+++ ReactJS/reactDemo/react-demo/src/components/TodoColorButton.js
@@ -5,50 +5,74 @@ const FavoritColor = () => {
     const [color, setColor] = useState('red');
     const [count, setCount] = useState(0);
     const counter = `My count number is ${count}!`;
+    let changeColor = 'red';
 
-    if (count < 2) {
-
-        return (
-            <>
-                <h1 style={{ color: 'red' }}>{counter}</h1>
-                <button
-                    type='button'
-                    onClick={() => setCount((count) => count + 1)}
-                >Counter</button>
-
-                <h1>My favorite color is {color}!</h1>
-                <button type='button'
-                    onClick={() => setColor('blue')}
-                >Blue</button>
-
-                <button
-                    type='button'
-                    onClick={() => setColor('red')}
-                >Red</button>
-            </>
-        )
-    } else {
-
-        return (
-            <>
-                <h1 style={{color: 'green'}}>{counter}</h1>
-                <button
-                    type='button'
-                    onClick={() => setCount((count) => count + 1)}
-                >Counter</button>
-
-                <h1>My favorite color is {color}!</h1>
-                <button type='button'
-                    onClick={() => setColor('blue')}
-                >Blue</button>
-
-                <button
-                    type='button'
-                    onClick={() => setColor('red')}
-                >Red</button>
-            </>
-        );
+    if(count > 2){
+        changeColor = 'green'
     }
+
+    return (
+        <>
+            <h1 style={{ color: changeColor }}>{counter}</h1>
+            <button
+                type='button'
+                onClick={() => setCount((count) => count + 1)}
+            >Counter</button>
+
+            <h1>My favorite color is {color}!</h1>
+            <button type='button'
+                onClick={() => setColor('blue')}
+            >Blue</button>
+
+            <button
+                type='button'
+                onClick={() => setColor('red')}
+            >Red</button>
+        </>
+    )
+    // if (count < 2) {
+
+    //     return (
+    //         <>
+    //             <h1 style={{ color: 'red' }}>{counter}</h1>
+    //             <button
+    //                 type='button'
+    //                 onClick={() => setCount((count) => count + 1)}
+    //             >Counter</button>
+
+    //             <h1>My favorite color is {color}!</h1>
+    //             <button type='button'
+    //                 onClick={() => setColor('blue')}
+    //             >Blue</button>
+
+    //             <button
+    //                 type='button'
+    //                 onClick={() => setColor('red')}
+    //             >Red</button>
+    //         </>
+    //     )
+    // } else {
+
+    //     return (
+    //         <>
+    //             <h1 style={{color: 'green'}}>{counter}</h1>
+    //             <button
+    //                 type='button'
+    //                 onClick={() => setCount((count) => count + 1)}
+    //             >Counter</button>
+
+    //             <h1>My favorite color is {color}!</h1>
+    //             <button type='button'
+    //                 onClick={() => setColor('blue')}
+    //             >Blue</button>
+
+    //             <button
+    //                 type='button'
+    //                 onClick={() => setColor('red')}
+    //             >Red</button>
+    //         </>
+    //     );
+    // }
 };
 
 export default FavoritColor;
