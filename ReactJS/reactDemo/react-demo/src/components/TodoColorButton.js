e45ffd17b46b1a419f0455d15e04f778d9f66fb6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-08 10:49:59 +0200

    Make change

--- ReactJS/reactDemo/react-demo/src/components/TodoColorButton.js
+++ ReactJS/reactDemo/react-demo/src/components/TodoColorButton.js
@@ -6,48 +6,48 @@ const FavoritColor = () => {
     const [count, setCount] = useState(0);
     const counter = `My count number is ${count}!`;
 
-    if(count < 2){
-     
-        return(
+    if (count < 2) {
+
+        return (
             <>
-            <h1>{counter}</h1>
-            <button
-        type='button'
-        onClick={() => setCount((count) => count + 1)}
-        >Counter</button>
+                <h1 style={{ color: 'red' }}>{counter}</h1>
+                <button
+                    type='button'
+                    onClick={() => setCount((count) => count + 1)}
+                >Counter</button>
 
-        <h1>My favorite color is {color}!</h1>
-        <button type='button'
-        onClick={() => setColor('blue')}
-        >Blue</button>
+                <h1>My favorite color is {color}!</h1>
+                <button type='button'
+                    onClick={() => setColor('blue')}
+                >Blue</button>
 
-        <button
-        type='button'
-        onClick={() => setColor('red')}
-        >Red</button>
-        </>
+                <button
+                    type='button'
+                    onClick={() => setColor('red')}
+                >Red</button>
+            </>
         )
-    }else{
+    } else {
 
-    return (
-        <>
-             <h1 color='green'>{counter}</h1>
-            <button
-        type='button'
-        onClick={() => setCount((count) => count + 1)}
-        >Counter</button>
+        return (
+            <>
+                <h1 style={{color: 'green'}}>{counter}</h1>
+                <button
+                    type='button'
+                    onClick={() => setCount((count) => count + 1)}
+                >Counter</button>
 
-        <h1>My favorite color is {color}!</h1>
-        <button type='button'
-        onClick={() => setColor('blue')}
-        >Blue</button>
+                <h1>My favorite color is {color}!</h1>
+                <button type='button'
+                    onClick={() => setColor('blue')}
+                >Blue</button>
 
-        <button
-        type='button'
-        onClick={() => setColor('red')}
-        >Red</button>
-        </>
-    );
+                <button
+                    type='button'
+                    onClick={() => setColor('red')}
+                >Red</button>
+            </>
+        );
     }
 };
 
