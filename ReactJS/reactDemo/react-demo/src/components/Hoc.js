567aa04c22fb36c7aec9fb0ee152393a64710a61
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-09 09:46:03 +0200

    Make change

--- ReactJS/reactDemo/react-demo/src/components/Hoc.js
+++ ReactJS/reactDemo/react-demo/src/components/Hoc.js
@@ -4,10 +4,11 @@ function reducer(arr, func){
     let result = arr[0];
 
     for(let nexElement of arr.slice(1))
-    
+
     result = func(result, nexElement);
 
     return result;
 };
 
-reducer([5, 10, 20], (a, b) => a + b);
+//reducer([5, 10, 20], (a, b) => a + b);
+reducer([5, 10, 20], (a, b) => a * b);
