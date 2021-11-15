6f7942c9a33ba78df826a59fcf365e4efc3c4235
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-15 11:02:42 +0200

    Craete header component

--- ReactJS/GamePlay/client/src/App.js
+++ ReactJS/GamePlay/client/src/App.js
@@ -1,23 +1,11 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <div id="box">
 
-        <header>
-          <h1><a class="home" href="#">GamesPlay</a></h1>
-          <nav>
-            <a href="#">All games</a>
-            <div id="user">
-              <a href="#">Create Game</a>
-              <a href="#">Logout</a>
-            </div>
-            <div id="guest">
-              <a href="#">Login</a>
-              <a href="#">Register</a>
-            </div>
-          </nav>
-        </header>
+       <Header />
 
         <main id="main-content">
         </main>
