10aa535cb7480a30c9ca6c88cc559dffa997a10a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-15 13:54:05 +0200

    Change

--- ReactJS/GamePlay/client/src/components/Header.js
+++ ReactJS/GamePlay/client/src/components/Header.js
@@ -1,38 +1,36 @@
 
-
-
-
 const Header = ({
   navigationChangeHandler,
 }) => {
+
   const onHeaderClick = (e) => {
     e.preventDefault();
 
-    console.log(e.target.tagName);
+    console.log('clicked');
 
-    if(e.target.tagName == 'A'){
+    if (e.target.tagName == 'A') {
       console.log(e.target.href);
       let url = new URL(e.target.href)
       navigationChangeHandler(url.pathname)
     }
-};
+  };
 
-    return (
-        <header onClick={onHeaderClick}>
-        <h1><a class="home" href="/home">GamesPlay</a></h1>
-        <nav>
-          <a href="/games">All games</a>
-          <div id="user">
-            <a href="/create-game">Create Game</a>
-            <a href="/logout">Logout</a>
-          </div>
-          <div id="guest">
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-          </div>
-        </nav>
+  return (
+      <header onClick={onHeaderClick}>
+      <h1><a class="home" href="/home">GamesPlay</a></h1>
+      <nav>
+        <a href="/games">All games</a>
+        <div id="user">
+          <a href="/create-game">Create Game</a>
+          <a href="/logout">Logout</a>
+        </div>
+        <div id="guest">
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </div>
+      </nav>
       </header>
-    );
+  );
 };
 
 export default Header;
