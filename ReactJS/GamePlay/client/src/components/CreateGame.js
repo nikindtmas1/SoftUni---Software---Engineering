53e75cc2e21330ade15186755765ed45781a9e9e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-17 12:21:52 +0200

    Change form handler

--- ReactJS/GamePlay/client/src/components/CreateGame.js
+++ ReactJS/GamePlay/client/src/components/CreateGame.js
@@ -5,23 +5,30 @@ import * as gameService from '../services/gameService';
 const CreateGame
  = ({history}) => {
 
-  const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const [maxLevel, setMaxLevel] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [summary, setSummary] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [category, setCategory] = useState("");
+  // const [maxLevel, setMaxLevel] = useState("");
+  // const [imageUrl, setImageUrl] = useState("");
+  // const [summary, setSummary] = useState("");
 
-  const onTitleChange = (e) => setTitle(e.target.value);
-  const onCategoryChange = (e) => setCategory(e.target.value);
-  const onMaxLevelChange = (e) => setMaxLevel(e.target.value);
-  const onImageUrlChange = (e) => setImageUrl(e.target.value);
-  const onSummaryChange = (e) => setSummary(e.target.value);
+  // const onTitleChange = (e) => setTitle(e.target.value);
+  // const onCategoryChange = (e) => setCategory(e.target.value);
+  // const onMaxLevelChange = (e) => setMaxLevel(e.target.value);
+  // const onImageUrlChange = (e) => setImageUrl(e.target.value);
+  // const onSummaryChange = (e) => setSummary(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    //const data = { title, category, maxLevel, imageUrl, summary };
+    let title = e.target.title.value;
+    let category = e.target.category.value;
+    let maxLevel = e.target.maxLevel.value;
+    let imageUrl = e.target.imageUrl.value;
+    let summary = e.target.summary.value;
+    
     const data = { title, category, maxLevel, imageUrl, summary };
-    console.log(data);
+    
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -35,25 +42,25 @@ const CreateGame
 
     return (
         <section id="create-page" class="auth">
-          <form id="create">
+          <form id="create" onSubmit={handleSubmit}>
             <div class="container">
 
               <h1>Create Game</h1>
               <label for="leg-title">Legendary title:</label>
-              <input onChange={onTitleChange} type="text" id="title" name="title" placeholder="Enter game title..." value={title} />
+              <input type="text" id="title" name="title" placeholder="Enter game title..." />
 
               <label for="category">Category:</label>
-              <input onChange={onCategoryChange} type="text" id="category" name="category" placeholder="Enter game category..." value={category} />
+              <input type="text" id="category" name="category" placeholder="Enter game category..." />
 
               <label for="levels">MaxLevel:</label>
-              <input onChange={onMaxLevelChange} type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" value={maxLevel} />
+              <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
 
               <label for="game-img">Image:</label>
-              <input onChange={onImageUrlChange} type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." value={imageUrl} />
+              <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
               <label for="summary">Summary:</label>
-              <textarea onChange={onSummaryChange} name="summary" id="summary" value={summary}>{summary}</textarea>
-              <input onClick={handleSubmit} class="btn submit" type="submit" value="Create Game" />
+              <textarea name="summary" id="summary" ></textarea>
+              <input class="btn submit" type="submit" value="Create Game" />
             </div>
           </form>
         </section>
