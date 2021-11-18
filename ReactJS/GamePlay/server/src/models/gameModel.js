70634ceb8f2691f09b83ccf43712e8ec7a5778f8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-18 20:21:10 +0200

    Added modules

--- ReactJS/GamePlay/server/src/models/gameModel.js
+++ ReactJS/GamePlay/server/src/models/gameModel.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
 
-    title: String,
-    category: String,
-    maxLevel: String,
-    imageUrl: String,
-    summary: String,
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    maxLevel: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
 });
 
 module.exports = mongoose.model('Game', gameSchema);
