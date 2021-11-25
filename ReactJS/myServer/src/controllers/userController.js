b84142abc1665487aa68c0bdda7e586de6339191
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-25 16:15:16 +0200

    Added user login register logic

---
+++ ReactJS/myServer/src/controllers/userController.js
@@ -0,0 +1,54 @@
+
+const router = require('express').Router();
+const services = require('../services/userService');
+
+router.post('/register', async (req, res, next) => {
+    let { username, password } = req.body;
+
+    try {
+
+       let user = await services.register({username, password});
+       let { accessToken, refreshToken } = await userService.login({ email, password });
+
+       res.json({
+           _id: user._id,
+           username: user.username,
+           accessToken,
+           refreshToken,
+       })
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    let { username, password } = req.body;
+
+    let { user, accessToken, refreshToken } = await services.login({ username, password });
+
+    res.json({
+        _id: user._id,
+        email: user.email,
+        accessToken,
+        refreshToken,
+    });
+});
+
+router.get('/logout', (req, res) => {
+    res.json({ ok: true });
+});
+
+router.post('/refresh', async (req, res) => {
+    console.log(req.body);
+    let refreshToken = req.body.refreshToken;
+
+    let { accessToken, refreshToken: newRefreshToken } = await services.refresh(refreshToken);
+
+    res.json({
+        accessToken,
+        refreshToken: newRefreshToken,
+    });
+});
+
+module.exports = router;
