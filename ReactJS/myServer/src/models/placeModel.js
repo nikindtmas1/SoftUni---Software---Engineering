60a1a8ff928e9ab50c2e2ee248fed58f0f2159d7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-26 13:53:45 +0200

    Create placeController

---
+++ ReactJS/myServer/src/models/placeModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+
+    title: String,
+    highlight: String,
+    gray: String,
+    imgUrl: String,
+    price: String,
+});
+
+module.exports = mongoose.model('Place', placeSchema);
