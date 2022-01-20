84b4cbf01fb7a3332b95376863064338b28b8ecf
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-20 15:31:47 +0200

    Added logic

--- TypeScript/ts-app-project/app.js
+++ TypeScript/ts-app-project/app.js
@@ -1,5 +1,18 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = 
 var person = {
     name: 'Nikolay',
-    age: 47
+    age: 47,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivites;
+var favoriteActivites1;
+favoriteActivites = ["Sport", ''];
+favoriteActivites1 = ["Sports", 1];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- TypeScript/ts-app-project/app.ts
+++ TypeScript/ts-app-project/app.ts
@@ -1,6 +1,28 @@
+// const person: {
+//     name: string;
+//     age: number;
+//      hobbies: String[];
+//      role: [number, String];
+// } = 
 const person = {
     name: 'Nikolay',
-    age: 47
+    age: 47,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'Author'],
 };
 
+// person.role.push('Admin');
+// person.role[1] = 10;
+
+// person.role = [3, 'Admin', 'User']
+
+let favoriteActivites: string[];
+let favoriteActivites1: any[];
+favoriteActivites = ["Sport", ''];
+favoriteActivites1 = ["Sports", 1];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
