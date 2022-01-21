40d5a2592465c09ba0e1e6e97b7b4fc089a02b4a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-21 13:32:20 +0200

    Added file examples

--- TypeScript/ts-app-project/app.js
+++ TypeScript/ts-app-project/app.js
@@ -1,22 +1,9 @@
-function combine(input1, input2, resultConvertion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConvertion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+function add(n1, n2) {
+    return n1 + n2;
 }
 ;
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-var combineNames = combine('Niki', 'Vili', 'as-string');
-console.log(combineNames);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+;
+printResult(add(5, 12));
--- TypeScript/ts-app-project/app.ts
+++ TypeScript/ts-app-project/app.ts
@@ -1,33 +1,9 @@
-function combine(
-    input1: number | string, 
-    input2: number | string,
-    resultConvertion: string,
-    ) {
-
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-
-        result = input1 + input2;
-
-    }else{
-
-       result = input1.toString() + input2.toString()
-    }
-
-    if(resultConvertion === 'as-number'){
-        return +result;
-    }else{
-
-        return result.toString();
-    }
-
+function add(n1:number, n2: number) {
+    return n1 + n2;
 };
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function printResult(num: number): void {
+    console.log("Result: " + num);
+};
 
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-
-const combineNames = combine('Niki', 'Vili', 'as-string');
-console.log(combineNames);
+printResult(add(5, 12))
