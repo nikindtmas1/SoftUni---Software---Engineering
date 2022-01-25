0eee8dd0a18be9c182f10c540e2d15e43c58562b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-25 21:21:50 +0200

    Added logic

--- TypeScript/invite-people-to-party/src/App.tsx
+++ TypeScript/invite-people-to-party/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string
+    url: string
+    age: number
+    note?: string
+  }[]
+}
+
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([])
+
+  people.map(person => {
+   console.log(person.name);
+    
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
     </div>
   );
 }
