9db83526a7c2ac764edf424faed870888c8188ea
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-26 20:17:48 +0200

    Change logic

--- TypeScript/invite-people-to-party/src/components/List.tsx
+++ TypeScript/invite-people-to-party/src/components/List.tsx
@@ -10,7 +10,7 @@ interface IProps {
 
 const List: React.FC<IProps> = ({ people }) => {
 
-    const renderList = () => {
+    const renderList = (): JSX.Element[] => {
 
         return people.map((person) => {
             return (
@@ -28,19 +28,7 @@ const List: React.FC<IProps> = ({ people }) => {
 
     return (
     <ul>
-        {/* {renderList} */}
-      {people.map(person => {
-          return (
-            <li className="List">
-            <div className="List-header">
-                <img className="List-img" src={person.url} alt=""/>
-                <h2>{person.name}</h2>
-            </div>
-            <p>{person.age} years old</p>
-            <p className="List-note">{person.note}</p>
-        </li>
-          )
-      })}
+        {renderList()}
     </ul>
     );
 };
