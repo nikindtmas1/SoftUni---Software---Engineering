00a36feadeacbea7908859261b0c960e24dfa87c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-26 20:39:53 +0200

    Create input logic

--- TypeScript/invite-people-to-party/src/App.tsx
+++ TypeScript/invite-people-to-party/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import List from './components/List';
+import AddToList from './components/AddToList';
 
 interface IState {
   people: {
@@ -26,6 +27,7 @@ function App() {
     <div className="App">
       <h1>People Invited to my Party</h1>
       <List people={people} />
+      <AddToList />
     </div>
   );
 }
---
+++ TypeScript/invite-people-to-party/src/components/AddToList.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const AddToList = () => {
+
+    const [input, setInput] = useState({
+        name: '',
+        age: '',
+        img: '',
+        note: ''
+    });
+
+    const handleChange = (e) => {
+
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    return (
+        <div className='AddToList'>
+            <input
+                type="text"
+                placeholder='Name'
+                className='AddToList-input'
+                value={input.name}
+                onChange={handleChange}
+                name='name'
+            />
+            <input
+                type="text"
+                placeholder='Age'
+                className='AddToList-input'
+                value={input.age}
+                onChange={handleChange}
+                name='age'
+
+            />
+            <input
+                type="text"
+                placeholder='Image url'
+                className='AddToList-input'
+                value={input.img}
+            />
+            <textarea
+                placeholder='Note'
+                className='AddToList-input'
+                value={input.note}
+                onChange={handleChange}
+                name='note'
+
+            />
+        </div>
+    );
+};
+
+export default AddToList;
