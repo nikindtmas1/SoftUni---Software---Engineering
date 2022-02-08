8de8a2f57fe56c1c5acf3ca9f6199f164fbcd3fe
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-08 13:51:39 +0200

    Added interface state

--- TypeScript/typeScript-sports-icons/client/src/App.tsx
+++ TypeScript/typeScript-sports-icons/client/src/App.tsx
@@ -2,16 +2,24 @@ import React, {useState} from 'react';
 
 import './App.css';
 
+export interface IState  {
+  people: {
+    name: string,
+    age: number,
+    description: string,
+    img: string
+  }
+}
 
 
 function App() {
 
 
-  const [people, setPeople] = useState({
+  const [people, setPeople] = useState<IState["people"]>({
     name: 'Niki',
     age: 30,
     description: 'Great',
-
+    img: ''
   })
 
   return (
