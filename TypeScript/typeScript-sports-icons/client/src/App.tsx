ea5dc55b6d01bba2067643155100b1e0561228fc
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-08 16:32:29 +0200

    Update App.tsx

--- TypeScript/typeScript-sports-icons/client/src/App.tsx
+++ TypeScript/typeScript-sports-icons/client/src/App.tsx
@@ -2,32 +2,34 @@ import React, {useState} from 'react';
 
 import './App.css';
 
+import Lists from './components/Lists';
+
 export interface IState  {
   people: {
     name: string,
     age: number,
     description: string,
     img: string
-  }
+  }[]
 }
 
 
 function App() {
 
 
-  const [people, setPeople] = useState<IState["people"]>({
+  const [people, setPeople] = useState<IState["people"]>([
+    {
     name: 'Niki',
     age: 30,
     description: 'Great',
     img: ''
-  })
+  }])
 
   return (
     <div className="App">
       <>
-        <h1>{people.name}</h1>
-        <p>{people.age}</p>
-        <p>{people.description}</p>
+        <h1>Sport Icons</h1>
+        <Lists people={people}></Lists>
       </>
     </div>
   );
