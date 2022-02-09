839dd4f0c7594b0e87f30beb2a617a7770368379
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-02-09 11:07:39 +0200

    Added inputs

--- TypeScript/typeScript-sports-icons/client/src/components/AddToList.tsx
+++ TypeScript/typeScript-sports-icons/client/src/components/AddToList.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const AddToList = () => {
-  return <div>Added Person</div>;
+    return (
+        <div className='AddToList'>
+            <h2>Add Person</h2>
+            <input className='AddToList-input' placeholder='Name'></input>
+            <input className='AddToList-input' placeholder='Image'></input>
+            <input className='AddToList-input' placeholder='Age'></input>
+            <textarea className='AddToList-input' placeholder='Description'></textarea>
+        </div>);
 };
 
 export default AddToList;
