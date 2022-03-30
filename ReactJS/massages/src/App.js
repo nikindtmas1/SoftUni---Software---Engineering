672d649da81e9de860f5556fe1c2ec572b57dbfb
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-03-30 09:43:51 +0300

    Added route

--- ReactJS/massages/src/App.js
+++ ReactJS/massages/src/App.js
@@ -1,6 +1,11 @@
 //import Navigation from "./components/Navigation";
 import React from 'react';
 import SearchAppBar from './components/AppBar';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import ServicesPage from './components/ServicesPage/ServicesPage';
+import TherapiesPage from './components/TherapiesPage/TherapiesPage';
+import OurStaffPage from './components/OurStaffPage/OurStaffPage';
 //import {Link} from 'react-router-dom';
 
 
@@ -13,7 +18,12 @@ function App() {
         {/* <h1>Hello world</h1> */}
         <SearchAppBar />
       </header>
-     
+     <Switch>
+        <Route path='/' component={HomePage} />
+        <Route path='/services' component={ServicesPage} />
+        <Route path='/bodyTreatments' component={TherapiesPage} />
+        <Route path='/staff' component={OurStaffPage} />
+     </Switch>
     </div>
   );
 }
