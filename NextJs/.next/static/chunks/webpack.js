c14d3f14dec413faf2a3a29bf9ef045bff4355f3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-20 11:48:50 +0200

    Added Link to home page

--- NextJs/.next/static/chunks/webpack.js
+++ NextJs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08c59c85be3002f7"; }
+/******/ 		__webpack_require__.h = function() { return "990c403b53d4a068"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
