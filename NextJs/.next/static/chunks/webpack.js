bda6372cbb5040ec53158bbb71cc71f476955edd
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-20 15:36:37 +0200

    Added global styles

--- NextJs/.next/static/chunks/webpack.js
+++ NextJs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a05de2a1c2a89d7"; }
+/******/ 		__webpack_require__.h = function() { return "ce92548b1b17738b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
