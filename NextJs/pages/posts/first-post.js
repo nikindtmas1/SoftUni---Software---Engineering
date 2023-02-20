ea21aa59e09bf45637cbc764e78ef8cddd2da6e4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-20 14:39:30 +0200

    Added public images

--- NextJs/pages/posts/first-post.js
+++ NextJs/pages/posts/first-post.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function FirstPost() {
     return (
         <>
+        <img style={{height:'230px', width: '200px'}} src="/images/profile.jpg" alt="My Name" />
         <h1>First Post</h1>
         <h2>
         <Link href='/'>Back to home page</Link>
