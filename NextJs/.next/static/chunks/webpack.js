0c0d134645d0529776d331d1e8ee846b2d346c70
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-21 10:05:49 +0200

    Update

--- NextJs/.next/static/chunks/webpack.js
+++ NextJs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f7b439f38f0dbafb"; }
+/******/ 		__webpack_require__.h = function() { return "55d03c4534b9b356"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
