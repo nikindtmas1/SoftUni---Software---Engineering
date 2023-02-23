ae7573c5d08f306d6b29b3007be83953c80989a7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-23 15:15:25 +0200

    Added function

--- NextJs/pages/posts/[id].js
+++ NextJs/pages/posts/[id].js
@@ -1,24 +1,28 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post() {
-    <Layout>
-
-    </Layout>
-};
+  <Layout></Layout>;
+}
 
 export async function getStaticPaths() {
-    // Return a list of possible value for id
-  };
+  // Return a list of possible value for id
+}
 
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
+}
 
-  export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-      paths,
-      fallback: false,
-    };
-  }
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
