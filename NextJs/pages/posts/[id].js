d6247361ac9ebf31d3d4fb0b0b910bc2ead77436
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-23 14:58:35 +0200

    Added function

--- NextJs/pages/posts/[id].js
+++ NextJs/pages/posts/[id].js
@@ -2,6 +2,10 @@ import Layout from "../../components/layout";
 
 export default function Post() {
     <Layout>
-        
+
     </Layout>
-}
+};
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+  }
